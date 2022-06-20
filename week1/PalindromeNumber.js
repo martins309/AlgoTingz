511415dbaf62a6a14766c094f7aba01dc8019dad
Andrew Martins <andrew_omartins@yahoo.com>
2022-06-20 18:26:04 -0400

    This is the inital commit for the algoz.

---
+++ week1/PalindromeNumber.js
@@ -0,0 +1,20 @@
+/**
+ * Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+ */
+
+/**
+ * Constraints:
+
+-231 <= x <= 231 - 1
+
+ */
+
+const isPalindrome = function(x){
+
+}
+
+//gimme ya best shot ...
---
+++ week5/MergeTwoSortedLists.js
@@ -0,0 +1,34 @@
+/**
+ * You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
+ */
+
+/**
+ Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+ */
+
+/**
+ * Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+ */
+
+const mergeTwoLists = function(list1, list2) {
+
+}
+
+//you got 30 seconds lil boi or girl..
