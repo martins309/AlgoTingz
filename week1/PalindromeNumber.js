dc6ddebbb09cca221e9c74179da360b5e929c2aa
Andrew Martins <andrew_omartins@yahoo.com>
2022-07-07 10:44:04 -0400

    first solution

--- week1/PalindromeNumber.js
+++ week1/PalindromeNumber.js
@@ -14,7 +14,23 @@ For example, 121 is a palindrome while 123 is not.
  */
 
 const isPalindrome = function(x){
-
+    if(x < 0) {
+        return false
+    }
+       
+       return x === reversedInteger(x);
+   };
+   
+   const reversedInteger = function(x) {
+       let reversed = 0;
+       
+       while (x > 0) {
+           reversed = (reversed * 10 ) + (x % 10);
+           x = Math.floor(x / 10);
+       }
+       
+       return reversed;
+     
 }
 
 //gimme ya best shot ...
