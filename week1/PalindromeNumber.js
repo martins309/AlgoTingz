7cb54ab5f6dba31e8c721532b2e968abcf913a60
Andrew Martins <andrew_omartins@yahoo.com>
2022-10-17 10:59:48 -0400

    .

--- week1/PalindromeNumber.js
+++ week1/PalindromeNumber.js
@@ -13,8 +13,40 @@ For example, 121 is a palindrome while 123 is not.
 
  */
 
-const isPalindrome = function(x){
+// const isPalindrome = function(x){
+//     if(x < 0) {
+//         return false
+//     }
 
-}
+//        return x === reversedInteger(x);
+//    };
+
+//    const reversedInteger = function(x) {
+//        let reversed = 0;
+
+//        while (x > 0) {
+//            reversed = (reversed * 10 ) + (x % 10);
+//            x = Math.floor(x / 10);
+//        }
+
+//        return reversed;
+
+// }
+
+const isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+  let origin = x;
+  let reversed = 0;
+
+  while (origin > 0) {
+    const lastDigit = origin % 10;
+    //taking the last digit and adding it to the ones column
+    reversed = reversed * 10 + lastDigit;
+    origin = Math.trunc(origin / 10);
+  }
+  return x === reversed;
+};
 
 //gimme ya best shot ...
