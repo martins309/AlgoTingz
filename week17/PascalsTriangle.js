1defaa177ad02b81dc7c3c526be366c685a9d708
Andrew Martins <andrew_omartins@yahoo.com>
2023-12-07 19:44:17 -0500

    writing out the correct answer using recursion

--- week17/PascalsTriangle.js
+++ week17/PascalsTriangle.js
@@ -21,24 +21,42 @@ Output: [[1]]
 1 <= numRows <= 30
  */
 
-const generate = function(numRows) {
-    while(numRows > 0) {
-        pascal = [[1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1]]
 
-        switch(pascal) {
-            case numRows == 1:
-                console.log(pascal[0])
-            case numRows == 2:
-                console.log(pascal[0][1])
-            case numRows == 3:
-                console.log(pascal[0][1][2])
-            case numRows == 4:
-                console.log(pascal[0][1][2])
-            case numRows == 5:
-                console.log([0][1][2][3][4])
-            default:
-                console.log("not in list")
-        }
+// this is wrong*****
+
+// const generate = function(numRows) {
+//     while(numRows > 0) {
+//         pascal = [[1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1]]
+
+//         switch(pascal) {
+//             case numRows == 1:
+//                 console.log(pascal[0])
+//             case numRows == 2:
+//                 console.log(pascal[0][1])
+//             case numRows == 3:
+//                 console.log(pascal[0][1][2])
+//             case numRows == 4:
+//                 console.log(pascal[0][1][2])
+//             case numRows == 5:
+//                 console.log([0][1][2][3][4])
+//             default:
+//                 console.log("not in list")
+//         }
+//     }
+
+// }
+
+
+//solution using recursion
+
+const generate = function(numRows) {
+    //if numrows == to 0 then return an empty array
+    if(numRows == 0){
+        return []
     }
 
+    //if the numrows == 1, then return [1] which is the first part of the pascals triangle
+    if(numRows == 1) {
+        return [1]
+    }
 }
