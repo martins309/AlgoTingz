8538c160c7ae8e5340c6f6505788ca81aa29ddc6
Andrew Martins <andrew_omartins@yahoo.com>
2024-09-02 10:47:15 -0400

    refactored the algorithm to use two pointer

--- Arrays/MergedSortedArray.js
+++ Arrays/MergedSortedArray.js
@@ -44,18 +44,29 @@ nums2.length == n
 // be aware of time and space complexity 
 
 var merge = function(nums1, m, nums2, n) {
-    if(nums1 || nums2 >= 0) {
-        return 
+    if(m <= 0 && n <= 0) {
+        return nums1
     }
-    for(i = n-1; i <= nums2.length; i--) {
-        for(j = m-1; j >= nums1.length; i--) {
-            if(nums2[j] > nums1[i]){
-                nums2.push(nums1)
-                
-            }
+
+    let i = m - 1
+    let j = n - 1
+    let k = m + n - 1
+
+    while(j >= 0) {
+        if(i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i]
+
+            i--
+        }else {
+            nums1[k] = nums1[j]
+            
+            j--
         }
+        k--
     }
-    return [nums1]
+    return nums1
+   
+    
    
 } 
 
