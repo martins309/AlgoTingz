2960105d5a75e3754006a9ed60c4e36525d500e5
Andrew Martins <andrew_omartins@yahoo.com>
2024-09-14 21:28:32 -0400

    solved the remove duplicate

--- Arrays/RemoveDuplicates.js
+++ Arrays/RemoveDuplicates.js
@@ -60,7 +60,7 @@ var  removeDuplicates = function (nums) {
     for(let j = 1; j < nums.length; j++) {
         if(nums[i] != nums[j]) {
             i++
-            nums[i] === nums[j]
+            nums[i] = nums[j]
         }
     }
     return i + 1
