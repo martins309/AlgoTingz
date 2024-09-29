c3aaabbb057f972792db3435eff90a9139d043ed
Andrew Martins <andrew_omartins@yahoo.com>
2024-09-28 21:12:43 -0400

    return numbers that are not even

--- Arrays/playground/forLoops.js
+++ Arrays/playground/forLoops.js
@@ -1,3 +1,13 @@
 let array = [1,2,3,3,4,54,56,67,3]
+let empty = []
 
+// for loops
 
+const loop = () => {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] / 2 !== 0){
+            array.push(empty)
+        }
+    }
+    return empty
+}
