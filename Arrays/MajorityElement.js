2ac5cff473ea63811c34fabaab477acbcd845797
Andrew Martins <andrew_omartins@yahoo.com>
2024-10-08 21:56:16 -0400

    iterate through the array to find the marjority element, three occurred most in the candidate therefore 3 must be the majority

--- Arrays/MajorityElement.js
+++ Arrays/MajorityElement.js
@@ -54,5 +54,14 @@ nevermind
 
 
 var majorityElement = function(nums){
-    
+    let candidate = null
+    let count = 0
+
+    for(let num of nums){
+        if(count === 0){
+            candidate = nums
+        }
+        count += (nums === candidate) ? 1 : -1
+    }
+    return candidate
 }
