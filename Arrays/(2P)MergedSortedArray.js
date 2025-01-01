f8910fdc4e4b0165e7a7751e90f3821721d14a38
Andrew Martins <andrew_omartins@yahoo.com>
2024-12-31 19:22:06 -0500

    refactored simpler solution

--- Arrays/(2P)MergedSortedArray.js
+++ Arrays/(2P)MergedSortedArray.js
@@ -43,35 +43,45 @@ nums2.length == n
 // this will also make me better javascript developer because I know how to use the tools better
 // be aware of time and space complexity 
 
-var merge = function(nums1, m, nums2, n) {
-    if(m <= 0 && n <= 0) {
-        return nums1
-    }
 
-    let i = m - 1
-    let j = n - 1
-    let k = m + n - 1
 
-    while(j >= 0) {
-        if(i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i]
+// var merge = function(nums1, m, nums2, n) {
+//     if(m <= 0 && n <= 0) {
+//         return nums1
+//     }
 
-            i--
-        }else {
-            nums1[k] = nums2[j]
+//     let i = m - 1
+//     let j = n - 1
+//     let k = m + n - 1
+
+//     while(j >= 0) {
+//         if(i >= 0 && nums1[i] > nums2[j]) {
+//             nums1[k] = nums1[i]
+
+//             i--
+//         }else {
+//             nums1[k] = nums2[j]
             
-            j--
-        }
-        k--
-    }
-    return nums1
+//             j--
+//         }
+//         k--
+//     }
+//     return nums1
    
-} 
+// } 
 
-let nums1 = [1,2,3,0,0,0]
-let nums2 = [2,5,6]
-let m = 3
-let n = 3
+// let nums1 = [1,2,3,0,0,0]
+// let nums2 = [2,5,6]
+// let m = 3
+// let n = 3
 
-console.log(merge(nums1, m, nums2, n))
+// console.log(merge(nums1, m, nums2, n))
 
+
+
+var merge = function(nums1, m, nums2, n) {
+    for(let i = m; j = 0; j < n, i++, j++) {
+        nums1[i] = nums2[j]
+    }
+    nums1.sort((a, b) => a - b)
+}
