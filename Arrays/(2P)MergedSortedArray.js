4fdf352f22e5b5c130a09ff736eb12091a40a604
Andrew Martins <andrew_omartins@yahoo.com>
2024-12-31 19:25:52 -0500

    .

--- Arrays/(2P)MergedSortedArray.js
+++ Arrays/(2P)MergedSortedArray.js
@@ -80,7 +80,7 @@ nums2.length == n
 
 
 var merge = function(nums1, m, nums2, n) {
-    for(let i = m; j = 0; j < n, i++, j++) {
+    for(let i = m, j = 0; j < n; i++, j++) {
         nums1[i] = nums2[j]
     }
     nums1.sort((a, b) => a - b)
