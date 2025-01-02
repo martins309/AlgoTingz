deb349590763684de7f80d7691ff876cb4dfbec3
Andrew Martins <andrew_omartins@yahoo.com>
2025-01-02 17:26:37 -0500

    refactored starting at position 1

--- Arrays/RemoveDuplicates.js
+++ Arrays/RemoveDuplicates.js
@@ -78,6 +78,9 @@ console.log(nums.slice(0, length))
 var removeDuplicates = function(nums) {
     let i = 1
     for(let j = 1; j < nums.length; j++) {
-        
+        if(nums[j] != nums[i - 1]){
+            nums[i] = nums[j]
+        }
     }
+    return i
 }
