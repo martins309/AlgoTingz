13e50f7beef0af895a59557f5c150e8878e6c8d5
Andrew Martins <andrew_omartins@yahoo.com>
2025-01-07 19:27:23 -0500

    created an object to store the values of the roman numerals

--- Arrays/RomanToInteger.js
+++ Arrays/RomanToInteger.js
@@ -49,14 +49,23 @@ It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 var RomanToInteger = function(s) {
     // s cannot = " " 
     // because then there is no number
-    
+const numerals = {    
+ I: 1,
+ V: 5,
+ X: 10,
+ L: 50,
+ C: 100,
+ D: 500,
+ M: 1000
+}
+
     
     if(s === "" ) {
         return "nothing is here"
     }
    for(let char of s){
     console.log(char)
-   
+
    }
 }
 console.log(RomanToInteger("MCMXCIV"))
