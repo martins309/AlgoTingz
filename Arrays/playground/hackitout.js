396a9184e5f88539550706f4880d2759e6f02ac2
Andrew Martins <andrew_omartins@yahoo.com>
2025-01-16 22:18:38 -0500

    notes for da ting

--- Arrays/playground/hackitout.js
+++ Arrays/playground/hackitout.js
@@ -1,3 +1,10 @@
 //this is where we will go when we need to work something out
 //this will help us understand our own syntax better cuz you suck lol
 
+// okay so this is the first time  using this to hack it out
+
+
+
+
+//here we will use this for the length of last word algo
+
