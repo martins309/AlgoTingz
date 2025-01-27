8c8775714c16458efedc10931b218c4b78c1dabd
Andrew Martins Jr. <75143566+martins309@users.noreply.github.com>
2025-01-27 15:37:00 +0000

    writing a test loop

--- Arrays/playground/forLoops.js
+++ Arrays/playground/forLoops.js
@@ -1,27 +1,35 @@
-let array = [1,2,3,3,4,54,56,67,3]
-let empty = []
+// let array = [1,2,3,3,4,54,56,67,3]
+// let empty = []
 
-// for loops
+// // for loops
 
-const loop = () => {
-    for(let i = 0; i < array.length; i++){
-        if(array[i] % 2 !== 0){
-            empty[empty.length] = array[i]
-        }
-    }
-    return empty.sort()
-}
-
-
-console.log(loop())
+// const loop = () => {
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] % 2 !== 0){
+//             empty[empty.length] = array[i]
+//         }
+//     }
+//     return empty.sort()
+// }
 
 
+// console.log(loop())
 
-const loop2 = () => {
-    for(num of array) {
-        console.log(num)
-    }
+
+
+// const loop2 = () => {
+//     for(num of array) {
+//         console.log(num)
+//     }
     
-}
+// }
 
-console.log(loop2())
+// console.log(loop2())
+
+let prices = [7, 6, 4, 3, 2, 1]
+
+const testLoop = () => {
+    for(let i = 0; i < prices.length; i++) {
+
+    }
+}
