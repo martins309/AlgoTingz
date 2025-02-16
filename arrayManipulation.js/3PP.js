cde3243255a54678baf4c0f4b05801e4c24ecc8a
Andrew Martins <andrew_omartins@yahoo.com>
2025-02-15 19:06:15 -0500

    three part partition problem

---
+++ arrayManipulation.js/3PP.js
@@ -0,0 +1,31 @@
+/*
+Three part partition
+
+Sure thing! Here's a slightly more challenging problem:
+
+---
+
+**Problem: Partition Array into Three Parts**
+  
+Given an array of integers, rearrange the array *in-place* such that:
+1. All negative numbers appear first.
+2. All zeros appear in the middle.
+3. All positive numbers appear at the end.
+
+The relative order of the numbers within their categories does not need to be maintained.  
+
+**Example Input:**  
+`[0, -1, 2, -3, 0, 4, -5]`  
+
+**Example Output:**  
+`[-1, -3, -5, 0, 0, 2, 4]`  
+
+---
+
+### Key Constraints:
+- Solve the problem in one pass (or as close to it as possible) using in-place swaps.
+- No extra arrays or data structures should be used.
+  
+Let me know your solution or if you’d like hints along the way!
+
+*/
