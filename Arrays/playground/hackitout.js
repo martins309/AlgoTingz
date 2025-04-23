95e4633ab27afc279dbb503b947b5573f1c5e937
Andrew Martins <andrew_omartins@yahoo.com>
2025-04-22 20:52:43 -0400

    umm idk lol

--- Arrays/playground/hackitout.js
+++ Arrays/playground/hackitout.js
@@ -178,9 +178,33 @@ Create a function that accepts a parameter representing a name and returns the m
 [Make sure you type the exact thing I wrote or the program may not execute properly]
 */
 
-function greeting(name){
-return `Hello, ${name} how are you doing today?`
+// function greeting(name){
+// return `Hello, ${name} how are you doing today?`
 
+// }
+
+// console.log(greeting("andrew"))
+
+/*
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+
+function basicOp(operation, value1, value2){
+  //Code
+  if(operation === " + "){
+    return ( value1, operation, value2)
+  }
+  
+  
 }
 
-console.log(greeting("andrew"))
+console.log(basicOp("+", 3,4))
