6306bea9a2f9a14ea908712f6bcda0da19376c49
Andrew Martins <andrew_omartins@yahoo.com>
2025-04-28 18:27:51 -0400

    .

--- Arrays/playground/colt.js
+++ Arrays/playground/colt.js
@@ -1,3 +1,7 @@
+//////////////////////////////FREQUENCY COUNTER TING///////////////////////////////
+
+
+
 /*
 Implement a function called countUniqueValues which accepts a sorted array
 and counts the unique values in the array. 
@@ -5,79 +9,95 @@ There can be negative numbers in the array
 but it will always be sorted
 */
 
-function countUniqueValues(array) {
-    let i = 0
+// function countUniqueValues(array) {
+//     let i = 0
 
-    for(let j = 1; j < array.length; j++){
-        if(array[i] !== array[j]){
-            i++
-            array[i] = array[j]
-    }
+//     for(let j = 1; j < array.length; j++){
+//         if(array[i] !== array[j]){
+//             i++
+//             array[i] = array[j]
+//     }
     
-    }
-    return i + 1
-}
-console.log(countUniqueValues([1,1,1,1,1,1,2]))
-// console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
-// console.log(countUniqueValues([-2,-1,-1,0,1]))
+//     }
+//     return i + 1
+// }
+// console.log(countUniqueValues([1,1,1,1,1,1,2]))
+// // console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
+// // console.log(countUniqueValues([-2,-1,-1,0,1]))
 
 
 
-//frequency counter 
 
-//given two strings write a function to determine if the second string is an anagram of the first. 
-//An anagram is a word, phrase, or name formed by rearranging the letter of another, such as cinema, formed from iceman
+// //given two strings write a function to determine if the second string is an anagram of the first. 
+// //An anagram is a word, phrase, or name formed by rearranging the letter of another, such as cinema, formed from iceman
 
-function anagram(str1, str2) {
-let ana = {}
-let gram = {}
+// function anagram(str1, str2) {
+// let ana = {}
+// let gram = {}
 
-str1 === ""  && str2 === "" ? false : true
+// str1 === ""  && str2 === "" ? false : true
 
-for(let s of str1) {
-  ana[s] = (ana[s] || 0) + 1
-}
+// for(let s of str1) {
+//   ana[s] = (ana[s] || 0) + 1
+// }
 
-for(let s of str2) {
-  gram[s] = (gram[s] || 0) + 1
-}
+// for(let s of str2) {
+//   gram[s] = (gram[s] || 0) + 1
+// }
 
-for(let key in ana) {
-  if(ana[key] !== gram[key]) {
-    return false 
-  }
-}
-  return true
-}
-console.log(anagram('qwerty', 'qeywrt'))
+// for(let key in ana) {
+//   if(ana[key] !== gram[key]) {
+//     return false 
+//   }
+// }
+//   return true
+// }
+// console.log(anagram('qwerty', 'qeywrt'))
 
-//lets refactor tomorrow 
+// //lets refactor tomorrow 
 
 
-// colts solution
+// // colts solution
 
-function validAnagram(anagram1, anagram2){
-  if(anagram1.length !== anagram2.length) {
-      return false;
-  }
+// function validAnagram(anagram1, anagram2){
+//   if(anagram1.length !== anagram2.length) {
+//       return false;
+//   }
 
   
-  const checkAnagram = {}
+//   const checkAnagram = {}
  
-  for(let i = 0; i < anagram1.length; i++) {
-      let letter = anagram1[i]
-      checkAnagram[letter] ? checkAnagram[letter] += 1 : checkAnagram[letter] = 1
-  }
+//   for(let i = 0; i < anagram1.length; i++) {
+//       let letter = anagram1[i]
+//       checkAnagram[letter] ? checkAnagram[letter] += 1 : checkAnagram[letter] = 1
+//   }
   
-  for(let i = 0; i < anagram2.length; i++) {
-      let letter = anagram2[i]
-      if(!checkAnagram[letter]){
-          return false
-      }else {
-          checkAnagram[letter] -= 1
-      }
-  }
-  return true
+//   for(let i = 0; i < anagram2.length; i++) {
+//       let letter = anagram2[i]
+//       if(!checkAnagram[letter]){
+//           return false
+//       }else {
+//           checkAnagram[letter] -= 1
+//       }
+//   }
+//   return true
+// }
+
+// validAnagram("tank", "knat");
+
+
+
+
+
+/////////////////Slinding Windows Ting/////////////////////////
+
+
+
+
+function slidingTing(array, num) {
+return array.length - num + 1
+
+
 }
 
-validAnagram("tank", "knat");
+console.log(slidingTing([1,2,3,4,5,6,7,8,9], 3))
