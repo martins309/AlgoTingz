df0b86f4e4f2ddd73400312deb81fdf6d70ee96c
Andrew Martins <andrew_omartins@yahoo.com>
2025-04-28 19:02:39 -0400

    this is the o(n^2) version of this

--- Arrays/playground/colt.js
+++ Arrays/playground/colt.js
@@ -93,11 +93,20 @@ but it will always be sorted
 
 
 
+//O(n^2)
 
-function slidingTing(array, num) {
-return array.length - num + 1
+function slidingTing(arr, num) {
+  if(num > arr.length) return null
 
-
+  let max = -Infinity
+  for(let i = 0; i < arr.length - num + 1; i++) {
+    let temp = 0
+    for(let j = 0; j < num; j++) {
+      temp += arr[i + j]
+    }
+    if(temp > max) {
+      max = temp
+    }
+  }
+  return max 
 }
-
-console.log(slidingTing([1,2,3,4,5,6,7,8,9], 3))
