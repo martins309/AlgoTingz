38eb38e438d35dcec8f9f64bd49eb27c6c2eb470
Andrew Martins <andrew_omartins@yahoo.com>
2025-05-02 22:07:10 -0400

    yes

--- Arrays/playground/coltPP.js
+++ Arrays/playground/coltPP.js
@@ -146,3 +146,33 @@ Space Complexity: O(N)
 // }
 
 // console.log(constructNote2("thisisfun", "thisisfun"))
+
+
+/*
+Given an array of positive integers, 
+some elements appear twice and others appear once. 
+Find all the elements that appear twice in this array. 
+Note that you can return the elements in any order.
+
+findAllDuplicates([4,3,2,7,8,2,3,1]) // array with 2 and 3
+findAllDuplicates([4, 3, 2, 1, 0]) // []
+findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3]) // array with 3, 2, and 1
+Time Complexity - O(n)
+*/
+
+
+
+function findAllDuplicates(arr){
+    
+
+    // array to hold all the duplicates
+    const duplicate = []
+    let i = 0
+    let j = arr.length -1
+
+   
+    
+
+}
+
+console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3]))
