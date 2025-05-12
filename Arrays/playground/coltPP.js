0a97e22902db9ba5e3cd8c6f41d251c06e7b199c
Andrew Martins <andrew_omartins@yahoo.com>
2025-05-11 20:28:36 -0400

    initalized the variables to find the max sum and the temp sum

--- Arrays/playground/coltPP.js
+++ Arrays/playground/coltPP.js
@@ -345,9 +345,11 @@ function firstUniqueChar(str) {
 
   /*
 
-  Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+  Given an array of integers and a number, write a function called maxSubarraySum, 
+  which finds the maximum sum of a subarray with the length of the number passed to the function.
 
-Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+Note that a subarray must consist of consecutive elements from the original array. 
+In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
 
 maxSubarraySum([100,200,300,400], 2) // 700
 maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)  // 39 
@@ -364,5 +366,7 @@ Space Complexity - O(1)
 
 
 function maxSubArray(arr) {
-  
+  let maxSum = 0
+  let tempSum = 0
+
 }
