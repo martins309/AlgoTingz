fa6b238e617a932a75a069dda0367e88ec0b95b5
Andrew Martins <andrew_omartins@yahoo.com>
2025-05-11 20:33:47 -0400

    I know that this isn't right but i wnant to understand how to use the sliding windows approach more

--- Arrays/playground/coltPP.js
+++ Arrays/playground/coltPP.js
@@ -365,8 +365,21 @@ Space Complexity - O(1)
   */
 
 
-function maxSubArray(arr) {
+function maxSubArraySum(arr) {
   let maxSum = 0
   let tempSum = 0
 
+  if(arr.length < num) return null 
+  for(let i = 0; i < num; i++){
+    maxSum+= arr[i]
+  }
+
+  tempSum = maxSum
+  for(let i = 0; i < arr.length; i++){
+    tempSum = tempSum - arr[i - num] + arr[i]
+    maxSum = Math.max(maxSum, tempSum)
+  }
+  return maxSum
+
 }
+console.log(maxSubArraySum([100,200,300,400]))
