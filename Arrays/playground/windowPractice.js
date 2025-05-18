40b316038ce3800b9385c109447eeadded35654f
Andrew Martins <andrew_omartins@yahoo.com>
2025-05-17 22:37:21 -0400

    finding the max average

--- Arrays/playground/windowPractice.js
+++ Arrays/playground/windowPractice.js
@@ -13,25 +13,34 @@ findAverages([1, 3, 2, 6, -1, 4, 1, 8, 2], 5);
 
 */
 
-function findAverages(arr, k){
-    let averages = []
-    let windowStart = 0
-    let windowSum = 0
+// function findAverages(arr, k){
+//     let averages = []
+//     let windowStart = 0
+//     let windowSum = 0
 
-    for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
-        windowStart+= arr[windowEnd]
+//     for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+//         windowStart+= arr[windowEnd]
 
-        if( windowEnd >= k - 1) {
-            averages.push(windowSum / k)
-        }
+//         if( windowEnd >= k - 1) {
+//             averages.push(windowSum / k)
+//         }
 
-        windowSum -= arr[windowStart]
+//         windowSum -= arr[windowStart]
 
-        windowStart++
+//         windowStart++
         
         
-    }
-    return averages
+//     }
+//     return averages
+// }
+
+// console.log(findAverages([1, 3, 2, 6, -1, 4, 1, 8, 2], 5))
+
+
+function maxAverage(arr, k) {
+    let maxAverage = -Infinity
+    
+
+
 }
 
-console.log(findAverages([1, 3, 2, 6, -1, 4, 1, 8, 2], 5))
