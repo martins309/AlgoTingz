32c3a05d12dc55465aa34064f888544f40e6703a
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-03 09:03:40 -0400

    created a variable

--- Arrays/playground/kata.js
+++ Arrays/playground/kata.js
@@ -351,10 +351,38 @@ solution('abc', 'd') // returns false
 
 
 
-function solutions(str, ending){
-    if(str === "") return null 
+// function solutions(str, ending){
+//     if(str === "") return null 
 
-    return str.slice(-ending.length) === ending
-  }
+//     return str.slice(-ending.length) === ending
+//   }
 
-  console.log(solutions("abc", "bc"))
+//   console.log(solutions("abc", "bc"))
+
+
+
+
+
+
+/*
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
+No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
+*/
+
+
+function sumTwoSmallestNumbers(numbers) {  
+  // Code here
+  if(numbers.length === 0) return null
+
+  let sum = 0
+
+
+
+}
