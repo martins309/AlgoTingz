b53078c46a0090a228392f79ac16f10cc5592b92
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-03 10:56:26 -0400

    .

--- Arrays/playground/kata.js
+++ Arrays/playground/kata.js
@@ -399,13 +399,36 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 
 
-function sumTwoSmallestNumbers(numbers) {  
-  // Code here
-  if(numbers.length < 4) return null
+// function sumTwoSmallestNumbers(numbers) {  
+//   // Code here
+//   if(numbers.length < 4) return null
 
-  const sorted = numbers.sort((a,b) => a - b)
-  return sorted[0] + sorted[1]
+//   const sorted = numbers.sort((a,b) => a - b)
+//   return sorted[0] + sorted[1]
 
-}
+// }
 
-console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
+// console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
+
+
+
+/*
+Description:
+There is a bus moving in the city which takes and drops some people at each bus stop.
+
+You are provided with a list (or array) of integer pairs. 
+Elements of each pair represent the number of people that get on the bus (the first item) 
+and the number of people that get off the bus (the second item) at a bus stop.
+
+Your task is to return the number of people who are still on the bus after 
+the last bus stop (after the last array). Even though it is the last bus stop, 
+the bus might not be empty and some people might still be inside the bus, 
+they are probably sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people 
+in the bus is always >= 0. So the returned integer can't be negative.
+
+The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+*/
