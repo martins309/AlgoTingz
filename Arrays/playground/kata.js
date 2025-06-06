e4f69d73ed748af1ff330645be248c830551d23c
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-06 11:50:58 -0400

    lets see what happens

--- Arrays/playground/kata.js
+++ Arrays/playground/kata.js
@@ -537,10 +537,10 @@ function openOrSenior(data){
         let handicap = data[0][1]
         let category = []
 
-        
-        
+        age >=55 && handicap >= 7 ? category.push("Senior") :  category.push("Open")
         
     }
+    return category
 }
 
 console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
