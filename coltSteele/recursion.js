9dea925fda85510a07497985110e3808d7ac4da8
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-10 13:16:52 -0400

    moving things around so that we are organized

--- coltSteele/recursion.js
+++ coltSteele/recursion.js
@@ -84,7 +84,21 @@ do not worry about negative bases and exponents.
 function power(base, exponent){
     if(exponent === 0) return 1
 
-    return base * power(base, exponent - 1 )
+    let same = 0
+    
+    let product = 1
+
+    if(exponent === base){
+        same = exponent * base
+        
+    }
+
+    if(exponent > base){
+        for(let i = 0; i < exponent; i++)
+            product *= base
+    }
+
+    return product
 
 }
 
