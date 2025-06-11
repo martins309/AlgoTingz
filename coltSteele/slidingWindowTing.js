1281be2d20f86e36f2b3dee59eb18dcb9e352107
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-11 08:28:10 -0400

    moved some things and now to see if this works

--- coltSteele/slidingWindowTing.js
+++ coltSteele/slidingWindowTing.js
@@ -74,27 +74,85 @@ Space Complexity - O(1)
   */
 
 
-function maxSubArraySum(arr, k) {
-  let maxSum = 0
-  let windowSum = 0
-  let windowStart = 0
+// function maxSubArraySum(arr, k) {
+//   let maxSum = 0
+//   let windowSum = 0
+//   let windowStart = 0
 
-  if(arr.length < k) return null 
+//   if(arr.length < k) return null 
 
-  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++){
-    windowSum += arr[windowEnd]  // add the next element
+//   for (let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+//     windowSum += arr[windowEnd]  // add the next element
 
-    //slide the window if we've hit the window size of k
-    //if we're at the third position or the kth position, start comparing
-    if(windowEnd >= k - 1) {
-      maxSum = Math.max(maxSum, windowSum)  //update max 
-      windowSum -= arr[windowStart] // subtract the window going out 
-      windowStart++ //slide the window forward 
+//     //slide the window if we've hit the window size of k
+//     //if we're at the third position or the kth position, start comparing
+//     if(windowEnd >= k - 1) {
+//       maxSum = Math.max(maxSum, windowSum)  //update max 
+//       windowSum -= arr[windowStart] // subtract the window going out 
+//       windowStart++ //slide the window forward 
+//     }
+//   }
+
+// return maxSum
+
+// }
+
+
+/*
+🧪 Practice: Find Average of All Contiguous Subarrays of Size K
+Prompt:
+Given an array of integers and a number k, 
+return an array of the averages of all contiguous subarrays of size k.
+
+
+// Example:
+findAverages([1, 3, 2, 6, -1, 4, 1, 8, 2], 5); 
+// Expected Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+
+
+*/
+
+// function findAverages(arr, k){
+//     let averages = []
+//     let windowStart = 0
+//     let windowSum = 0
+
+//     for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+//         windowStart+= arr[windowEnd]
+
+//         if( windowEnd >= k - 1) {
+//             averages.push(windowSum / k)
+//         }
+
+//         windowSum -= arr[windowStart]
+
+//         windowStart++
+        
+        
+//     }
+//     return averages
+// }
+
+// console.log(findAverages([1, 3, 2, 6, -1, 4, 1, 8, 2], 5))
+
+
+function maxAverage(arr, k) {
+    let maxAv = -Infinity
+    let windowSum = 0
+    let windowStart = 0
+
+    for (let i = windowEnd; i < arr.length; i++) {
+        windowSum += arr[windowEnd]
+
+        if(windowEnd >= k - 1){
+            maxAv = Math.max(maxAv, windowSum / k)
+            windowSum -= arr[windowStart]
+            windowStart++
+        }
     }
-  }
 
-return maxSum
-
+    return maxAv
 }
+console.log(maxAverage([1, 3, 2, 6, -1, 4, 1, 8, 2], 5))
 
 
