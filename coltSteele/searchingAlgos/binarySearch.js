682bc96116c79698ef98663a306acc8e41dbdf82
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-23 12:57:19 -0400

    now trying the binary search

---
+++ coltSteele/searchingAlgos/binarySearch.js
@@ -0,0 +1,25 @@
+/*
+write a function that accepts a sorted array and a value
+create a pointer for the left part of the array
+and one for the right part of the array
+while the left pointer comes before the right pointer
+create a pointer for the middle
+if you find the value that you want return the middle
+if the value is too small then move the left poninter up
+if the value is to large you move the right pointer down
+if you dont find the value return -1 
+
+*/
+
+function binarySearch(arr, value){
+
+    let left = arr[0]
+    let right = arr.length -1
+
+    while(left < right){
+        let middle = Math.floor(right - left / 2)
+        
+
+    }
+
+}
