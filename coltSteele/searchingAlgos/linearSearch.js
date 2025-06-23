f9de62e9cc213227afa45aa0c416ce65d0f318db
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-23 12:26:10 -0400

    lets see if i made this work lol

--- coltSteele/searchingAlgos/linearSearch.js
+++ coltSteele/searchingAlgos/linearSearch.js
@@ -1,5 +1,21 @@
 /*
 write a function that accepts an array and a value
 loop through the array and check to see if that element is equal 
-to the current value
+to the current value. if it is return the index it was found
+else return -1
 */
+
+
+
+function linearSearch(arr, value){
+    if(arr === 0 ) return -1 
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === value){
+            return indexOf(arr[i])
+        }
+    }
+    return -1
+}
+
+console.log(linearSearch([1,2,3,4,5,6,3], 4))
