2a8992ef6faf8509a79a519196834985227dc3a9
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-24 12:05:30 -0400

    naive string search

---
+++ coltSteele/searchingAlgos/naiveStringSearch.js
@@ -0,0 +1,15 @@
+/*
+define a function called  string search with parameters that 
+have the long string and the pattern we are looking for
+loop over a longer string 
+if the characters don't match break out of the inner loop
+if you complete the loop and find a match, increment a count
+return the count
+
+*/
+
+
+function stringSearch(str, pat){
+    
+    let count = 0
+}
