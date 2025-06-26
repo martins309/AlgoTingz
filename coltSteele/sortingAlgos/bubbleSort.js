394bf0f1648ebe395589068650a00a500ca7dffd
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-26 08:35:45 -0400

    the start of bubble sort

---
+++ coltSteele/sortingAlgos/bubbleSort.js
@@ -0,0 +1,12 @@
+/*
+define a function called bubble sort takes an arr of nums
+start looping with a variable called i at the end of the array 
+towards the beginning
+then create an inner loop outside of the outer loop called j 
+from the beginning until i -1 
+if arr[j] is greater than arr[j + 1] swap em 
+return the sorted arr
+
+
+
+*/
