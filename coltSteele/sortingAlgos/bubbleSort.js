bf812fdc227602f2df31cc92b351db55b8a4dff2
Andrew Martins <andrew_omartins@yahoo.com>
2025-06-26 08:52:23 -0400

    i got it rightgs

--- coltSteele/sortingAlgos/bubbleSort.js
+++ coltSteele/sortingAlgos/bubbleSort.js
@@ -14,9 +14,9 @@ return the sorted arr
 
 function bubbleSort(arr){
     
-    for(let i = 0; i < arr.length - 1; i++){
-        for(let j = 0; j < i - 1; j++){
-             if(arr[j] > arr[j +1 ]){
+    for(let i = arr.length - 1; i >= 0; i--){
+        for(let j = 0; j < arr.length; j++){
+             if(arr[j] > arr[j + 1]){
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
              }
         }
