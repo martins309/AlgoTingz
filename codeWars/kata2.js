6f9c2174234fe1f1abcdd85266cca093e4fdf8c8
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-01 22:20:40 -0400

    this is probably wrong

--- codeWars/kata2.js
+++ codeWars/kata2.js
@@ -19,3 +19,25 @@ Examples
 // }
 
 // console.log(reverseString("I like eggs"))
+
+
+
+
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+*/
+
+
+function numToString(num){
+    return num.toString()
+}
+console.log(numToString(123))
+
