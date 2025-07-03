dbc14605f0095134ae59c7ab159afbe72e366841
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-02 22:48:35 -0400

    selection ting

--- coltSteele/sortingAlgos/selectionSort.js
+++ coltSteele/sortingAlgos/selectionSort.js
@@ -1,8 +1,19 @@
 /*
 store first element as the smallest value that you've seen
 compare this item to the next item in the array until you find a smaller number
-
+if the smaller number is found, designate the smaller number to tbe the new min and 
+continue until the end of the array
+if the minimum is the value index you initially began with
+swap the two values
+repeat this with the next element until the array is sorted
 
 
 
 */
+
+
+function selectionSort(arr){
+    let min = 0
+
+    
+}
