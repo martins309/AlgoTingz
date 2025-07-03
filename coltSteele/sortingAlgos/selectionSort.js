3b198d494c26b65075afff27e3736b5c36a4dfd2
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-02 22:54:15 -0400

    this is not right but im trying lol

--- coltSteele/sortingAlgos/selectionSort.js
+++ coltSteele/sortingAlgos/selectionSort.js
@@ -1,7 +1,7 @@
 /*
 store first element as the smallest value that you've seen
 compare this item to the next item in the array until you find a smaller number
-if the smaller number is found, designate the smaller number to tbe the new min and 
+if the smaller number is found, designate the smaller number to be the new min and 
 continue until the end of the array
 if the minimum is the value index you initially began with
 swap the two values
@@ -15,5 +15,13 @@ repeat this with the next element until the array is sorted
 function selectionSort(arr){
     let min = 0
 
-    
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] < min) {
+            min = arr[i]
+        }
+        if(arr[i + 1] < min){
+            min = arr[i + 1]
+        }
+    }
+
 }
