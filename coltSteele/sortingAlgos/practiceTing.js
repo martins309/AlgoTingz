194bfd74ef5bef7e964fdbe5ab764bf867ba10f6
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-11 11:30:49 -0400

    just practicing the sliding windows pattern

--- coltSteele/sortingAlgos/practiceTing.js
+++ coltSteele/sortingAlgos/practiceTing.js
@@ -210,7 +210,15 @@ function maxSubArray(arr, num){
     let maxSum = 0
     let tempSum = 0
 
-    for(let i = 0; i < arr.length; i++){
+    for(let i = 0; i < num; i++){
+        maxSum += arr[i]
+    }
+    tempSum = maxSum
+    for(let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(tempSum, maxSum)
         
     }
+    return maxSum
 }
+console.log(maxSubArray([1,2,3,4,5], 3))
