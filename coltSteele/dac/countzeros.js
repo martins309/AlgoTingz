25c2d99a810b6f5aaadaa11b15382c79cffbcdb1
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-15 11:22:26 -0400

    welp

--- coltSteele/dac/countzeros.js
+++ coltSteele/dac/countzeros.js
@@ -14,16 +14,22 @@ function countZeros(arr){
     let left = 0
     let right = arr.length -1
     let count = 0
-
-
+    const sortedArr = arr.sort((a, b) => a - b)
+    
     while(left <= right){
         let middle = Math.floor((left + right) / 2)
-        
-        if(arr[middle] !== 0){
-            return count
+
+        if(arr[middle] === 0){
+            count++
+        }else if(arr[left] !== 0){
+            left = middle + 1
+        }else{
+            right = middle -1
         }
+        count++
+        
     }
-    
+    return count
 }
 
 console.log(countZeros([1,1,1,0,0,0]))
