4a305f863ebde9d316369754d8a4ab6daf078e26
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-16 22:20:10 -0400

    we will fix tomorrow

--- coltSteele/dac/sortedFrequency.js
+++ coltSteele/dac/sortedFrequency.js
@@ -6,14 +6,20 @@
 function sortedFrequency(arr, num) {
     let left = 0
     let right = arr.length - 1 
-    let frequency = {}
+    let count = 0
     
 
     while(left <= right){
         let middle = Math.floor((left + right) / 2)
-        const numbers = arr[frequency]
-        console.log(numbers)
+        if(arr[middle] === num) {
+            count = arr[middle]
+            right = middle - 1
+        }else {
+            left = middle + 1
+        }
         
     }
-
+    
 }
+
+console.log(sortedFrequency([1,1,2,2,2,2,3], 2))
