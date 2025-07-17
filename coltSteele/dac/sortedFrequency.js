fba7fa3678c9bb5c5b0788deba4a0aeeb132c3d7
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-16 22:05:40 -0400

    checking to see if the frequencies are being stored

--- coltSteele/dac/sortedFrequency.js
+++ coltSteele/dac/sortedFrequency.js
@@ -7,9 +7,13 @@ function sortedFrequency(arr, num) {
     let left = 0
     let right = arr.length - 1 
     let frequency = {}
+    
 
     while(left <= right){
         let middle = Math.floor((left + right) / 2)
+        const numbers = arr[frequency]
+        console.log(numbers)
+        
     }
 
 }
