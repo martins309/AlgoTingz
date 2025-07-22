a6aa4db5a80557235f0df3c28c6a15d997cd18fb
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-22 09:33:10 -0400

    re wrote the sorted frequency problem and now to check it

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -4,43 +4,96 @@
 
 
 
+// function sortedFrequency(arr, num){
+//     function findFirst() {
+//         let left = 0
+//         let right = arr.length -1 
+//         let result = -1
+
+//         while(left <= right){
+//             let middle = Math.floor((left + right) / 2)
+//             if(arr[middle] === num){
+//                 result = middle
+//                 right = middle - 1
+//             } else if (arr[middle] < num){
+//                 left = mid + 1
+//             } else{
+//                 right = middle - 1
+//             }
+//         }
+//         return result
+//     }
+    
+//     function findLast(){
+//         let left = 0 
+//         let right = arr.length -1
+//         let result = -1
+
+//         while(left <= right){
+//             let middle = Math.floor((left + right ) / 2)
+//             if(arr[middle] === num){
+//                 result = middle
+//                 left = middle + 1
+//             }else if(arr[middle] < num){
+//                 left = middle + 1 
+//             }else {
+//                 right = middle - 1
+//             }
+//         }
+//         return result 
+//     }
+
+//     let first = findFirst()
+//     if(first === -1) return -1
+//     let last = findLast()
+
+//     return last - first + 1
+// }
+
+
+
+
+
+
+//look ma no hands lol
+
 function sortedFrequency(arr, num){
-    function findFirst() {
-        let left = 0
-        let right = arr.length -1 
+    function findFirst(arr, num){
+        let first = 0
+        let end = arr.length -1
         let result = -1
 
         while(left <= right){
             let middle = Math.floor((left + right) / 2)
             if(arr[middle] === num){
                 result = middle
-                right = middle - 1
-            } else if (arr[middle] < num){
-                left = mid + 1
-            } else{
+                right = middle -1
+            }else if(arr[middle] < num){
+                left = middle + 1
+            }else {
                 right = middle - 1
             }
         }
         return result
     }
-    
-    function findLast(){
-        let left = 0 
-        let right = arr.length -1
+
+    function findLast(arr, num){
+        let first = 0 
+        let last = arr.length - 1
         let result = -1
 
         while(left <= right){
-            let middle = Math.floor((left + right ) / 2)
+            let middle = Math.floor((left + right) / 2)
             if(arr[middle] === num){
                 result = middle
                 left = middle + 1
             }else if(arr[middle] < num){
-                left = middle + 1 
+                left = middle + 1
             }else {
                 right = middle - 1
             }
         }
-        return result 
+        return result
     }
 
     let first = findFirst()
@@ -49,4 +102,10 @@ function sortedFrequency(arr, num){
 
     return last - first + 1
 }
+
+console.log()
+
+
+
+
 
