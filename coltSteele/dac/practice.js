2f1729feb92dca2d9dc30bf805513b4e48fa64c7
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-24 11:10:24 -0400

    last time

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -57,37 +57,103 @@
 
 //look ma no hands lol
 
-function sortedFrequency(arr, num){
-    function occ1(){
+// function sortedFrequency(arr, num){
+//     function occ1(){
+//         let left = 0
+//         let right = arr.length - 1
+//         let result = -1
+
+//         while(left <= right ){
+//             let mid = Math.floor((left + right ) / 2)
+
+//             if(arr[mid] === num){
+//                 result = mid
+//                 right = mid - 1
+//             }else if (arr[mid] < num){
+//                 left = mid + 1
+//             }else {
+//                 right = mid - 1
+//             }
+//         }
+//         return result
+//     }
+//     function occ2(){
+//         let left= 0 
+//         let right = arr.length - 1
+//         let result = -1
+
+//         while(left <= right){
+//             let mid = Math.floor((left + right ) / 2)
+
+//             if(arr[mid] === num){
+//                 result = mid
+//                 left = mid + 1
+//             }else if(arr[mid] < num){
+//                 left = mid + 1
+//             }else {
+//                 right = mid - 1
+//             }
+//         }
+//         return result
+//     }
+
+//     let first = occ1()
+//     if(first === -1) return -1
+//     let last = occ2()
+
+//     return last - first + 1
+// }
+
+// console.log(sortedFrequency([1,3,4,4,4,4,5,6,7], 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//start here
+
+function sortedFreak(arr, num){
+    function num1(){
         let left = 0
         let right = arr.length - 1
         let result = -1
 
-        while(left <= right ){
-            let mid = Math.floor((left + right ) / 2)
-
+        while(left <= right){
+            let mid = Math.floor((left + right) / 2)
             if(arr[mid] === num){
                 result = mid
-                right = mid - 1
-            }else if (arr[mid] < num){
+                right = middle - 1
+            }else if(arr[mid] < num){
                 left = mid + 1
             }else {
                 right = mid - 1
             }
         }
-        return result
+      return result
     }
-    function occ2(){
-        let left= 0 
+
+    function num2(){
+        let left = 0 
         let right = arr.length - 1
         let result = -1
 
-        while(left <= right){
-            let mid = Math.floor((left + right ) / 2)
-
+        while(left <= right) {
+            let mid = Math.floor((left + right) / 2)
             if(arr[mid] === num){
                 result = mid
-                left = mid + 1
             }else if(arr[mid] < num){
                 left = mid + 1
             }else {
@@ -97,14 +163,12 @@ function sortedFrequency(arr, num){
         return result
     }
 
-    let first = occ1()
+    let first = num1()
     if(first === -1) return -1
-    let last = occ2()
+    let last = num2()
 
     return last - first + 1
 }
-
-console.log(sortedFrequency([1,3,4,4,4,4,5,6,7], 4))
 
 
 
