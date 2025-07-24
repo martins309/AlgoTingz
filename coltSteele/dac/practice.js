c5c5b950541d0e43a6f8f401f2c52b888b535f3d
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-24 11:19:50 -0400

    I understand that completely and I wil try other methods to get different results

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -125,53 +125,53 @@
 
 //start here
 
-function sortedFreak(arr, num){
-    function num1(){
-        let left = 0
-        let right = arr.length - 1
-        let result = -1
+// function sortedFreak(arr, num){
+//     function num1(){
+//         let left = 0
+//         let right = arr.length - 1
+//         let result = -1
 
-        while(left <= right){
-            let mid = Math.floor((left + right) / 2)
-            if(arr[mid] === num){
-                result = mid
-                right = mid - 1
-            }else if(arr[mid] < num){
-                left = mid + 1
-            }else {
-                right = mid - 1
-            }
-        }
-      return result
-    }
+//         while(left <= right){
+//             let mid = Math.floor((left + right) / 2)
+//             if(arr[mid] === num){
+//                 result = mid
+//                 right = mid - 1
+//             }else if(arr[mid] < num){
+//                 left = mid + 1
+//             }else {
+//                 right = mid - 1
+//             }
+//         }
+//       return result
+//     }
 
-    function num2(){
-        let left = 0 
-        let right = arr.length - 1
-        let result = -1
+//     function num2(){
+//         let left = 0 
+//         let right = arr.length - 1
+//         let result = -1
 
-        while(left <= right) {
-            let mid = Math.floor((left + right) / 2)
-            if(arr[mid] === num){
-                result = mid
-                left = mid + 1
-            }else if(arr[mid] < num){
-                left = mid + 1
-            }else {
-                right = mid - 1
-            }
-        }
-        return result
-    }
+//         while(left <= right) {
+//             let mid = Math.floor((left + right) / 2)
+//             if(arr[mid] === num){
+//                 result = mid
+//                 left = mid + 1
+//             }else if(arr[mid] < num){
+//                 left = mid + 1
+//             }else {
+//                 right = mid - 1
+//             }
+//         }
+//         return result
+//     }
 
-    let first = num1()
-    if(first === -1) return -1
-    let last = num2()
+//     let first = num1()
+//     if(first === -1) return -1
+//     let last = num2()
 
-    return last - first + 1
-}
+//     return last - first + 1
+// }
 
-console.log(sortedFreak([1,2,3,4,5,5,5,5,5,6,7,8,15,34,56,78,88,99], 5))
+// console.log(sortedFreak([1,2,3,4,5,5,5,5,5,6,7,8,15,34,56,78,88,99], 5))
 
 
 
