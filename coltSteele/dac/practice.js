e513595d7a257e13a8049cff0953db4ddf602a3e
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-24 10:42:23 -0400

    i under stand this algorithm completely and now I will write it without mistake, then I'll try the recursive call

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -57,53 +57,7 @@
 
 //look ma no hands lol
 
-function sortedFrequency(arr, num){
-    function findFirst(){
-        let left = 0
-        let right = arr.length -1
-        let result = -1
 
-        while(left <= right){
-            let middle = Math.floor((left + right) / 2)
-            if(arr[middle] === num){
-                result = middle
-                right = middle - 1
-            }else if(arr[middle] < num){
-                left = middle + 1
-            }else {
-                right = middle - 1
-            }
-        }
-        return result
-    }
-
-    function findLast(){
-        let left = 0 
-        let right = arr.length - 1
-        let result = -1
-
-        while(left <= right){
-            let middle = Math.floor((left + right) / 2)
-            if(arr[middle] === num){
-                result = middle
-                left = middle + 1
-            }else if(arr[middle] < num){
-                left = middle + 1
-            }else {
-                right = middle - 1
-            }
-        }
-        return result
-    }
-
-    let first = findFirst()
-    if(first === -1) return -1
-    let last = findLast()
-
-    return last - first + 1
-}
-
-console.log(sortedFrequency([1,2,2,2,2,2,3], 2))
 
 
 
