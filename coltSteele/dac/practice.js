4177135829e0b4dfb90cf1d8a955d88fe095ef55
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-24 10:51:28 -0400

    im pretty sure this will return 4

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -57,7 +57,54 @@
 
 //look ma no hands lol
 
+function sortedFrequency(arr, num){
+    function occ1(){
+        let left = 0
+        let right = arr.length - 1
+        let result = -1
 
+        while(left <= right ){
+            let mid = Math.floor((left + right ) / 2)
+
+            if(arr[mid] === num){
+                result = mid
+                right = middle - 1
+            }else if (arr[mid] < num){
+                left = mid + 1
+            }else {
+                right = mid -1
+            }
+        }
+        return result
+    }
+    function occ2(){
+        let first = 0 
+        let last = arr.length - 1
+        let result = -1
+
+        while(left <= right){
+            let mid = Math.floor((left + right ) / 2)
+
+            if(arr[mid] === num){
+                result = mid
+                left = mid + 1
+            }else if(arr[mid] < num){
+                left = mid + 1
+            }else {
+                right = mid - 1
+            }
+        }
+        return result
+    }
+
+    let first = occ1()
+    if(first === -1) return -1
+    let last = occ2()
+
+    return occ2 - occ1 + 1
+}
+
+console.log(sortedFrequency([1,3,4,4,4,4,5,6,7], 4))
 
 
 
