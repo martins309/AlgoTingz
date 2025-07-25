444459f75dcd73a33a32100845070532aed65ef9
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-25 11:08:44 -0400

    go check the rest of the right side and if not check the rest of the left side. Then do the same but check the rest of the left first and then the right

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -179,39 +179,86 @@
 
 //recursive call 
 
-function sortedFrequency(arr, num) {
+// function sortedFrequency(arr, num) {
+//     let firstIdx = findFirst(arr, num)
+//     if(firstIdx === -1) return -1
+//     let lastIdx = findLast(arr, num)
+//     return lastIdx - firstIdx + 1
+
+
+//     function findFirst(low = 0, high = arr.length -1 ){
+//         if(high >= low) {
+//         let mid = Math.floor((left + right) / 2)
+//             if((mid === 0 || num > arr[mid - 1]) && arr[mid] === num ){
+//                 return mid
+//             } else if(num > arr[mid]){
+//                 return findFirst(arr, num, mid + 1, high)
+//             } else {
+//                 return findFirst(arr, num, low, mid - 1)
+//             }
+//         }
+//         return -1 
+//     }
+
+//     function findLast(low = 0, high = arr.length - 1){
+//         if(high >= low){
+//             let mid = Math.floor((left + right) / 2)
+//             if(arr[mid] === arr.length - 1 || num < arr[mid + 1] && arr[mid] === num) {
+//                 return mid
+//             }else if(num < arr[mid] ){
+//                 return findLast(arr, num, low, mid -1)
+//             } else {
+//                 return findLast(arr, num, mid + 1, high)
+//             }
+//         }
+       
+//     }
+//     return -1 
+// }
+
+
+
+
+
+
+
+
+function sortedFrequency(arr, num){
     let firstIdx = findFirst(arr, num)
-    if(firstIdx === -1) return -1
+    if(first === - 1) return -1
     let lastIdx = findLast(arr, num)
+
     return lastIdx - firstIdx + 1
 
-
-    function findFirst(low = 0, high = arr.length -1 ){
-        if(high >= low) {
-        let mid = Math.floor((left + right)/ 2)
-            if((mid === 0 || num > arr[mid - 1]) && arr[mid] === num ){
-                return mid
-            } else if(num > arr[mid]){
-                return findFirst(arr, num, mid + 1, high)
-            } else {
-                return findFirst(arr, num, low, mid - 1)
-            }
+    function findFirst(low = 0, high = arr.length - 1){
+        while(high >= left){
+        let mid = Math.floor((left + right) / 2)
+        if(mid = 0 || num > arr[mid - 1] && arr[mid] === num){
+            return mid 
+        }else if(num > arr[mid]){
+            return findFirst(arr, num, mid + 1, high)
+        }else {
+            return findFirst(arr, num, low, mid - 1)
         }
+
+        }
+        
         return -1 
     }
 
     function findLast(low = 0, high = arr.length - 1){
-        if(high >= low){
-            let mid = Math.floor((left + right) / 2)
-            if(arr[mid] === arr.length - 1 || num < arr[mid + 1] && arr[mid] === num) {
+        while(high >= left){
+            let mid = Math.floor((left + high) / 2)
+            if(mid = arr.length -1 || num < arr[mid + 1] && arr[mid] === num){
                 return mid
-            }else if(num < arr[mid] ){
-                return findLast(arr, num, low, mid -1)
-            } else {
+            }else if(num < arr[mid]){
+                return findLast(arr, num, low, mid - 1)
+            }else {
                 return findLast(arr, num, mid + 1, high)
             }
         }
-       
     }
-    return - 1 
+    return - 1
+
 }
+
