f122a7dce1d2a628f1600f619b875e8b5fd141dd
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-26 17:09:26 -0400

    yup

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -225,15 +225,15 @@
 
 function sortedFrequency(arr, num){
     let firstIdx = findFirst(arr, num)
-    if(first === - 1) return -1
+    if(firstIdx === - 1) return -1
     let lastIdx = findLast(arr, num)
 
     return lastIdx - firstIdx + 1
 
-    function findFirst(low = 0, high = arr.length - 1){
+    function findFirst(arr, num, low = 0, high = arr.length - 1){
         while(high >= low){
         let mid = Math.floor((low + high) / 2)
-        if(mid = 0 || num > arr[mid - 1] && arr[mid] === num){
+        if(mid === 0 || num > arr[mid - 1] && arr[mid] === num){
             return mid 
         }else if(num > arr[mid]){
             return findFirst(arr, num, mid + 1, high)
@@ -246,10 +246,10 @@ function sortedFrequency(arr, num){
         return -1 
     }
 
-    function findLast(low = 0, high = arr.length - 1){
+    function findLast(arr, num, low = 0, high = arr.length - 1){
         while(high >= low){
             let mid = Math.floor((low + high) / 2)
-            if(mid = arr.length -1 || num < arr[mid + 1] && arr[mid] === num){
+            if(mid === arr.length -1 || num < arr[mid + 1] && arr[mid] === num){
                 return mid
             }else if(num < arr[mid]){
                 return findLast(arr, num, low, mid - 1)
