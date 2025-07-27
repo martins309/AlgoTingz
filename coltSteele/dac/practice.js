e40e1efeb4d96bd1e8b533037faaaa2ec46fed3f
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-27 16:35:30 -0400

    we will study this and add to our arsenal

--- coltSteele/dac/practice.js
+++ coltSteele/dac/practice.js
@@ -223,43 +223,92 @@
 
 
 
-function sortedFrequency(arr, num){
-    let firstIdx = findFirst(arr, num)
-    if(firstIdx === - 1) return -1
-    let lastIdx = findLast(arr, num)
+// function sortedFrequency(arr, num){
+//     let firstIdx = findFirst(arr, num)
+//     if(firstIdx === - 1) return -1
+//     let lastIdx = findLast(arr, num)
 
-    return lastIdx - firstIdx + 1
+//     return lastIdx - firstIdx + 1
 
-    function findFirst(arr, num, low = 0, high = arr.length - 1){
-        while(high >= low){
-        let mid = Math.floor((low + high) / 2)
-        if(mid === 0 || num > arr[mid - 1] && arr[mid] === num){
-            return mid 
-        }else if(num > arr[mid]){
-            return findFirst(arr, num, mid + 1, high)
-        }else {
-            return findFirst(arr, num, low, mid - 1)
-        }
+//     function findFirst(arr, num, low = 0, high = arr.length - 1){
+//         while(high >= low){
+//         let mid = Math.floor((low + high) / 2)
+//         if(mid === 0 || num > arr[mid - 1] && arr[mid] === num){
+//             return mid 
+//         }else if(num > arr[mid]){
+//             return findFirst(arr, num, mid + 1, high)
+//         }else {
+//             return findFirst(arr, num, low, mid - 1)
+//         }
 
-        }
+//         }
         
-        return -1 
-    }
+//         return -1 
+//     }
 
-    function findLast(arr, num, low = 0, high = arr.length - 1){
-        while(high >= low){
-            let mid = Math.floor((low + high) / 2)
-            if(mid === arr.length -1 || num < arr[mid + 1] && arr[mid] === num){
-                return mid
-            }else if(num < arr[mid]){
-                return findLast(arr, num, low, mid - 1)
-            }else {
-                return findLast(arr, num, mid + 1, high)
-            }
-        }
-    }
-    return - 1
+//     function findLast(arr, num, low = 0, high = arr.length - 1){
+//         while(high >= low){
+//             let mid = Math.floor((low + high) / 2)
+//             if(mid === arr.length -1 || num < arr[mid + 1] && arr[mid] === num){
+//                 return mid
+//             }else if(num < arr[mid]){
+//                 return findLast(arr, num, low, mid - 1)
+//             }else {
+//                 return findLast(arr, num, mid + 1, high)
+//             }
+//         }
+//     }
+//     return - 1
 
+// }
+
+// console.log(sortedFrequency([1,2,3,4,4,4,4,5,6,7,8,9], 4))
+
+
+
+
+
+
+
+//you had to find the pivot point first and then work with that
+
+function findRotatedIndex(array, num) {
+  var pivot = findPivot(array)
+  if (pivot > 0 && num >= array[0] && num <= array[pivot - 1]) {
+    return binarySearch(array, num, 0, pivot - 1);
+  } else {
+    return binarySearch(array, num, pivot, array.length - 1);
+  }
 }
-
-console.log(sortedFrequency([1,2,3,4,4,4,4,5,6,7,8,9], 4))
+ 
+function binarySearch(array, num, start = 0, end = array.length - 1) {
+  if (array.length === 0) return -1;
+  if (num < array[start] || num > array[end]) return -1;
+ 
+  while (start <= end) {
+    var mid = Math.floor((start + end) / 2);
+    if (array[mid] === num) {
+      return mid;
+    } else if (num < array[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+ 
+function findPivot(arr) {
+  if (arr.length === 1 || arr[0] < arr[arr.length - 1]) return 0;
+  var start = 0
+  var end = arr.length - 1;
+  while (start <= end) {
+    var mid = Math.floor((start + end) / 2);
+    if (arr[mid] > arr[mid + 1]) return mid + 1
+    else if (arr[start] <= arr[mid]) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+  }
+}
