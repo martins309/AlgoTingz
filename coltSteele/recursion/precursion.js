5e10f9c7aecfcc4add686d1bde3556543b0785aa
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-03 19:40:21 -0400

    test

--- coltSteele/recursion/precursion.js
+++ coltSteele/recursion/precursion.js
@@ -1,3 +1,26 @@
 // I have an idea of how this works so im going to try to write it
 //myself
 
+
+
+function collectOddValues(arr) {
+
+    let result = []
+
+    function helper(helperInput) {
+        
+        if(helperInput.length === 0) {
+            return
+        }
+
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+
+    }
+    helper(arr)
+
+    return result
+}
+
+console.log(collectOddValues([1,3,4,5,6,7,78,89,9,8,5]))
