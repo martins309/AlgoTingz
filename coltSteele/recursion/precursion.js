6328a6499611eb073fcb94702faf666e1b4d37e7
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-04 11:24:41 -0400

    practice ting

--- coltSteele/recursion/precursion.js
+++ coltSteele/recursion/precursion.js
@@ -3,26 +3,45 @@
 
 
 
-function collectOddValues(arr) {
+// function collectOddValues(arr) {
 
+//     let result = []
+
+//     function helper(helperInput) {
+        
+//         if(helperInput.length === 0) {
+//             return
+//         }
+
+//         if(helperInput[0] % 2 !== 0){
+//             result.push(helperInput[0])
+//         }
+
+//         helper(helperInput.slice(1))
+
+//     }
+//     helper(arr)
+
+//     return result
+// }
+
+// console.log(collectOddValues([1,3,4,5,6,7,78,89,9,8,5]))
+
+
+
+
+
+function collectOddValues(arr){
+    
     let result = []
 
-    function helper(helperInput) {
-        
-        if(helperInput.length === 0) {
+    function helper(helperInput){
+        if(helperInput.length === 0){
             return
         }
 
         if(helperInput[0] % 2 !== 0){
-            result.push(helperInput[0])
+            result.push()
         }
-
-        helper(helperInput.slice(1))
-
     }
-    helper(arr)
-
-    return result
 }
-
-console.log(collectOddValues([1,3,4,5,6,7,78,89,9,8,5]))
