831153be2b6bce33f25180d1bff80ef8cdb9f672
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-04 11:34:54 -0400

    test

--- coltSteele/recursion/precursion.js
+++ coltSteele/recursion/precursion.js
@@ -41,7 +41,15 @@ function collectOddValues(arr){
         }
 
         if(helperInput[0] % 2 !== 0){
-            result.push()
+            result.push(helperInput[0])
         }
+
+        helper(helperInput.slice(1))
     }
+
+    helper(arr)
+    
+    return result
 }
+
+console.log(collectOddValues([1,2,3,4,5,6,7,8,9,0,9,8,7,6]))
