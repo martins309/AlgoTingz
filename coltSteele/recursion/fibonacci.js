f4296667cc6955b8c868af198c92d9ea825986a0
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-06 13:21:12 -0400

    im gonna hack this out until im absolutely stuck and drained

--- coltSteele/recursion/fibonacci.js
+++ coltSteele/recursion/fibonacci.js
@@ -20,11 +20,23 @@ sequence: 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597
 // fib(28) // 317811
 // fib(35) // 9227465
 
+//okay so the issue is that they're giving a number that represents that value at that position
+// so the 4th num is 3
+//the 10th num is 55
+// and so on
+// who the fuck do i associate a position to a value? 
+// an array
+// but i can't just load the array with nums and then try to map it
+// it has to be a fibonacci array that continues to grow each time the function is called
+// and then when i console.log a num it returns the number that holds that position
+//but how? 
 function fib(num){
 
-    if(num === 0 || num === 1 ) return 1 
+    if(num === 0) return 0
+    if(num === 1) return 1 
 
-    fib()
+
+    
 
     
 }
