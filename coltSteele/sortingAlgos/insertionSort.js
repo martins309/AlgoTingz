a5943d983638f5acf2b869f644c6ef6d277cfc85
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-09 20:12:33 -0400

    comparator function for other data structures

--- coltSteele/sortingAlgos/insertionSort.js
+++ coltSteele/sortingAlgos/insertionSort.js
@@ -17,26 +17,40 @@ repeat until the array is sorted
 
 // like the enthusiam but this is most defintely a brute force method lol
 
-function insertionSort(arr){
-    if(arr.length === 0) return arr
+// function insertionSort(arr){
+//     if(arr.length === 0) return arr
 
+//     for(let i = 1; i < arr.length; i++){
+//         if(arr[i] < arr[i - 1]){
+//             [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]]
+//         }
+//         if(arr[i + 1] < ar[i]){
+//             [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]]
+//         }
+//         for(let j = 0; j < arr.length; j++){
+//             if(arr[j] > arr[j + 1]){
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+//             }
+//             [arr[i + 2], arr[i + 1]] = [arr[i + 1], arr[i + 2]]
+//         }
+//     }
+//     return arr
+// }
+// console.log(insertionSort([4,9,2,4,0,7,5]))
+//                           [4,2,9,4,0,7,4]
+//                           [2,4,9,4,0,7,4]
+//                           [2,4,4,9,0,7,4]
+
+
+
+
+function insertionSort(arr){
     for(let i = 1; i < arr.length; i++){
-        if(arr[i] < arr[i - 1]){
-            [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]]
+        let currentVal = arr[i]
+        
+        for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+           arr[j + 1] = arr[j]  
         }
-        if(arr[i + 1] < ar[i]){
-            [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]]
-        }
-        for(let j = 0; j < arr.length; j++){
-            if(arr[j] > arr[j + 1]){
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-            }
-            [arr[i + 2], arr[i + 1]] = [arr[i + 1], arr[i + 2]]
-        }
+        arr[j + 1] = currentVal
     }
-    return arr
 }
-console.log(insertionSort([4,9,2,4,0,7,5]))
-                          [4,2,9,4,0,7,4]
-                          [2,4,9,4,0,7,4]
-                          [2,4,4,9,0,7,4]
