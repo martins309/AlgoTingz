030064f2f2e8ac4d31d1c09c239120ee5d6f4a24
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-17 13:48:16 -0400

    now to add the comparator

--- coltSteele/sortingAlgos/sortingPractice.js
+++ coltSteele/sortingAlgos/sortingPractice.js
@@ -8,7 +8,7 @@ function insertionSort(arr){
         let j = i - 1
 
         while(j >= 0 && arr[j] > currentVal){
-            arr[j + 1] = j
+            arr[j + 1] = arr[j]
             j--
         }
         arr[j + 1] = currentVal
