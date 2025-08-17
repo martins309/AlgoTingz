17736c5ca08db3357ed84b66a5201421006cf038
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-17 13:43:19 -0400

    insertion sort with a while loop

--- coltSteele/sortingAlgos/sortingPractice.js
+++ coltSteele/sortingAlgos/sortingPractice.js
@@ -2,18 +2,14 @@
 
 
 
-function insertionSort(arr) {
+function insertionSort(arr){
     for(let i = 1; i < arr.length; i++){
         let currentVal = arr[i]
         let j = i - 1
 
-        while( j >= 0 && arr[j] > currentVal){
-            arr[j + 1] = arr[j]
+        while(j >= 0 && arr[j] > currentVal){
+            arr[j + 1] = j
             j--
         }
-        arr[j + 1] = currentVal
     }
-    return arr
 }
-
-console.log(insertionSort([4,6,8,3,1,99]))
