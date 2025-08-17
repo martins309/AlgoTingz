ef425d6afdc966890d1e36bffdcaba28243e54da
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-16 22:26:19 -0400

    yeah boi

--- coltSteele/sortingAlgos/insertionSort.js
+++ coltSteele/sortingAlgos/insertionSort.js
@@ -49,7 +49,7 @@ function insertionSort(arr){
         var currentVal = arr[i]
         
         for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--){
-           arr[j + 1] = arr[j]  
+            arr[j + 1] = arr[j]  
 
         }
         arr[j + 1] = currentVal
--- coltSteele/sortingAlgos/sortingPractice.js
+++ coltSteele/sortingAlgos/sortingPractice.js
@@ -2,17 +2,15 @@
 
 
 
-function insertionSort(arr){
-    for(let i = 1; i < arr.length; i++){
+function insertionSort(arr) {
+    for(let  i  = 1; i < arr.length; i++){
         let currentVal = arr[i]
         let j
 
-        for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--){
-            arr[j + 1] = j
+        for(j = 1 - 1; j >= 0 && arr[j] > currentVal; j--){
+            arr[j + 1] = arr[j]
         }
         arr[j + 1] = currentVal
     }
     return arr
 }
-
-console.log(insertionSort([4,6,8,9,7,3,2,1,4,3]))
