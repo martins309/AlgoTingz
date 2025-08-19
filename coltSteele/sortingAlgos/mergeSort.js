36b93b127b32a48522d52d85516499f0974f3877
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-19 18:12:02 -0400

    merge sort ting

---
+++ coltSteele/sortingAlgos/mergeSort.js
@@ -0,0 +1,23 @@
+/*
+create an empty array  that you'll return at the end
+and take a look at the smallest values in each input array
+do while loops use counters call i and j
+they need to start at zero
+while there are still values that we haven't looked at not at the end of the array
+
+
+we are going to compare the item in the first array to the item in the second array
+if the item in the first array is smaller than the item in the second array
+push the value of the first array into the results and move on the the next
+value in the first array
+
+If the value of the first array is larger than the value of second array
+push the value of the seconds array into the results and move on to the next
+item in the second array
+
+one we exhaust on array, push in all remaining values from the other array
+
+
+
+
+*/
