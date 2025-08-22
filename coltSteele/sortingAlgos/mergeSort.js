9de2a7d2b6de48acd22239dc686678d96366b2e9
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-22 08:50:10 -0400

    I think this will work if not we fix

--- coltSteele/sortingAlgos/mergeSort.js
+++ coltSteele/sortingAlgos/mergeSort.js
@@ -36,6 +36,14 @@ function mergeArr(arr1, arr2){
             j++
 
     }
+    while(i < arr1.length){
+        result.push(arr1[i])
+    }
+    
+    while(j < arr2.length){
+        result.push(arr2[j])
+    }
+    return result
 
 }
 
