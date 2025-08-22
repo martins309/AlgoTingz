f6be2180477106f0d915261ec056527f057f19ba
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-22 08:42:27 -0400

    merging the array the first part of merge sort

--- coltSteele/sortingAlgos/mergeSort.js
+++ coltSteele/sortingAlgos/mergeSort.js
@@ -22,30 +22,6 @@ one we exhaust on array, push in all remaining values from the other array
 
 */
 
-function mergeArr(arr1, arr2) {
-    let result = []
-    let i = 0
-    let j = 0
-
-    while(i < arr1.length && j < arr2.length) {
-        if(arr2[j] > arr1[i]){
-            result.push(arr1[i])
-            i++
-        } else {
-            result.push(arr2[j])
-            j++
-        }
-    }
-
-    while(i < arr1.length) {
-        result.push(arr[i])
-        i++
-    }
-     while(j < arr2.length){
-        result.push(arr2[j])
-        j++
-     }
-    return result
+function mergeArr(arr1, arr2){
+    
 }
-
-console.log(mergeArr([1,10,50], [2,14,99,100]))
