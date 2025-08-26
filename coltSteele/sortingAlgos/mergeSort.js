905e314fd8e43e0ec1c44e488256fd15e1d8ec76
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-26 10:00:50 -0400

    lets try this out

--- coltSteele/sortingAlgos/mergeSort.js
+++ coltSteele/sortingAlgos/mergeSort.js
@@ -32,7 +32,7 @@ function mergeArr(arr1, arr2, comparator){
     let i = 0
     let j = 0 
 
-    while(i < arr1.length && j < arr2.length){
+    while(comparator(i < arr1.length && j < arr2.length)){
         if(arr2[j] > arr1[i]){
             result.push(arr1[i])
             i++
@@ -55,7 +55,7 @@ function mergeArr(arr1, arr2, comparator){
 
 }
 
-// console.log(mergeArr([1, 5, 9, 43], [10, 23, 33, 98]))
+console.log(mergeArr([1, 5, 9, 43], [10, 23, 33, 98]))
 
 const comparator = (a, b) => 
     a > b ? 1 :
@@ -76,16 +76,16 @@ return the merge array
 */
 
 
-function mergeSort(arr){
-    if (arr.length <= 1) return arr
+// function mergeSort(arr){
+//     if (arr.length <= 1) return arr
 
-    let mid = Math.floor(arr.length / 2)
-    let left = mergeSort(arr.slice(0, mid))
-    let right = mergeSort(arr.slice(mid))
+//     let mid = Math.floor(arr.length / 2)
+//     let left = mergeSort(arr.slice(0, mid))
+//     let right = mergeSort(arr.slice(mid))
    
-   return mergeArr(left, right)
+//    return mergeArr(left, right)
 
     
-}
+// }
 
-console.log(mergeSort([10,24,76,73,72,1,9]))
+// console.log(mergeSort([10,24,76,73,72,1,9]))
