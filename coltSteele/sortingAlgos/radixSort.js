7e9f5aedcf317ba2e84ba59c2a31f2e8bb2acd04
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-29 23:13:32 -0400

    what number to the power of 10 gives us num? do Math.log10 + 1. The abs is for neg nums

--- coltSteele/sortingAlgos/radixSort.js
+++ coltSteele/sortingAlgos/radixSort.js
@@ -8,6 +8,12 @@ returns the digit in num at the given place value
 
 
 
-function getDigit(num, place){
-    return Math.floot(Math.abs(num) / Math.pow(10, i) % 10)
+function getDigit(num, i){
+    return Math.floor(Math.abs(num) / Math.pow(10, i) % 10)
+}
+
+function digitCount(num){
+    if (num === 0) return 1
+    return Math.floor(Math.log10(Math.abs(num))) + 1
+
 }
