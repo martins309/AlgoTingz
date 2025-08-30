6f0475583ef24b95297cc57b281620a7e14e381f
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-30 13:59:24 -0400

    comments and tings

--- coltSteele/sortingAlgos/radixSort.js
+++ coltSteele/sortingAlgos/radixSort.js
@@ -12,6 +12,7 @@ function getDigit(num, i){
     return Math.floor(Math.abs(num) / Math.pow(10, i) % 10)
 }
 
+//this method shows how many digits there are in a number
 function digitCount(num){
     if (num === 0) return 1
     return Math.floor(Math.log10(Math.abs(num))) + 1
@@ -20,8 +21,6 @@ function digitCount(num){
 
 //given an array of integers, return the number of digits 
 // in the largest numbers in the list
-
-
 function mostDigits(nums){
     let maxDigits = 0
     for(let i = 0; i < nums.length; i++){
