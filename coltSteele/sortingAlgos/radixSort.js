402b5b2706e2ae69438cc99cac4ddf5f6d72e4c7
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-29 23:08:14 -0400

    given a number take the position i times that by 10 to the power of i, take that number and mod by 10 and that is the digit

--- coltSteele/sortingAlgos/radixSort.js
+++ coltSteele/sortingAlgos/radixSort.js
@@ -4,3 +4,10 @@
 use the getDigit(num, place)
 returns the digit in num at the given place value
 */
+
+
+
+
+function getDigit(num, place){
+    return Math.floot(Math.abs(num) / Math.pow(10, i) % 10)
+}
