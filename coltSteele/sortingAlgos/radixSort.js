8066b477aad3dd3d8d3918b662ec66d97240da2c
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-05 23:08:12 -0400

    we're gonna practice all of the sorting algorithmns that we hav learend this far. Each time that we write one, we need to pay attention to why it works and what is happening and not just trying to remember the code

--- coltSteele/sortingAlgos/radixSort.js
+++ coltSteele/sortingAlgos/radixSort.js
@@ -15,61 +15,70 @@ returns the digit in num at the given place value
 // then you do mod 10 to get the answer
 //abs is for neg nums
 
+// function getDigit(num, i){
+//     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
+// }
+
+// //this method shows how many digits there are in a number
+// function digitCount(num){
+//     if (num === 0) return 1
+//     return Math.floor(Math.log10(Math.abs(num))) + 1
+
+// }
+
+// //given an array of integers, return the number of digits 
+// // in the largest numbers in the list
+// function mostDigits(nums){
+//     let maxDigits = 0
+//     for(let i = 0; i < nums.length; i++){
+//         maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+//     }
+//     return maxDigits
+// }
+
+
+
+// /*
+// define a function that accepts a list of numbers
+// figure out how many digits the largest number has
+// loop from k = 0 up to this largest number of digits
+
+// for each iteration of the loop
+//     create a bucket for each digit (0 to 9)
+//     place each number in the corresponding bucket
+//     based on its kth digit
+
+// Replace our existing array with the values in our buckets,
+// starting with 0 and going up to 9
+// return the list at the end
+// */
+
+
+// function radixSort(nums){
+
+//     let count = mostDigits(nums)
+
+//     for(let k = 0; k < count; k++){
+//         //creates a bucket for each digit
+//         let buckets = Array.from({ length: 10}, () => [])
+//         for(let i = 0; i < nums.length; i++){
+//             //put numbers in their bucket based on the position of the digit
+//             buckets[getDigit(nums[i], k)].push(nums[i])
+//         }
+//         //add them back together into a new array
+//         nums = [].concat(...buckets)
+//     }
+
+
+//     return nums
+
+// }
+
+
+
+
+
+
 function getDigit(num, i){
-    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
-}
-
-//this method shows how many digits there are in a number
-function digitCount(num){
-    if (num === 0) return 1
-    return Math.floor(Math.log10(Math.abs(num))) + 1
-
-}
-
-//given an array of integers, return the number of digits 
-// in the largest numbers in the list
-function mostDigits(nums){
-    let maxDigits = 0
-    for(let i = 0; i < nums.length; i++){
-        maxDigits = Math.max(maxDigits, digitCount(nums[i]))
-    }
-    return maxDigits
-}
-
-
-
-/*
-define a function that accepts a list of numbers
-figure out how many digits the largest number has
-loop from k = 0 up to this largest number of digits
-
-for each iteration of the loop
-    create a bucket for each digit (0 to 9)
-    place each number in the corresponding bucket
-    based on its kth digit
-
-Replace our existing array with the values in our buckets,
-starting with 0 and going up to 9
-return the list at the end
-*/
-
-
-function radixSort(nums){
-
-    let count = mostDigits(nums)
-
-    for(let k = 0; k < count; k++){
-        //creates a bucket for each digit
-        let buckets = Array.from({ length: 10}, () => [])
-        for(let i = 0; i < nums.length; i++){
-            //put numbers in their bucket based on the position of the digit
-            buckets[getDigit(nums[i], k)].push(nums[i])
-        }
-        //add them back together into a new array
-        nums = [].concat(...buckets)
-    }
-
-
-    return nums
-
+    
 }
