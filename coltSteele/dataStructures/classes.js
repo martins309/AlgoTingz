dfcb8908949004d8ecde5ec0b5dbaea7a1992d61
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-07 15:13:26 -0400

    classes will be fun

--- coltSteele/dataStructures/classes.js
+++ coltSteele/dataStructures/classes.js
@@ -13,17 +13,32 @@ class Student {
         this.lastName = lastName
         this.grade = year
         this.tardies = 0
+        this.scores = []
     }
     //you can create static methods that work on the instance of the class 
     fullName() {
         return `this is your full name:  ${this.firstName} ${this.lastName}`
     }
     markLate(){
+        this.tardies += 1
         if(this.tardies >= 3){
             return 'Your are expelled'
         }
-        this.tardies += 1
+        
         return `${this.firstName} has been late ${this.tardies} times `
+    }
+    addScores(score){
+        this.scores.push(score)
+        return this.scores
+    }
+
+    average(){
+        let sum = this.scores.reduce((a, b) => a + b)
+        return sum / this.scores.length
+    }
+
+    static enrollStudent(){
+        return "I have enrolled a student"
     }
    
 }
@@ -31,7 +46,15 @@ class Student {
 
 //when you want to instantiate an object you use the new keyword like so
 
-let firstStudent = new Student('chicken', 'little', 9)
-let secondStudent = new Student('your mom', 'is a hoe', '69')
+//when using a class method you cannot call it on an individual instance 
+// it is solely used as a utility of that class 
+// like when we are creating methods for models 
 
-console.log(secondStudent.markLate())
+let firstStudent = new Student('chicken', 'little', 9)
+let secondStudent = new Student('your mom', 'is a hoe', 69)
+
+secondStudent.addScores(94)
+secondStudent.addScores(99)
+secondStudent.addScores(55)
+
+console.log(Student.enrollStudent())
