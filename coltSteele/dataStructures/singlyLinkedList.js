d7d6b356c9fcf1811e0435e601a679e78696f9c0
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-08 12:45:32 -0400

    notes on singly linked list

---
+++ coltSteele/dataStructures/singlyLinkedList.js
@@ -0,0 +1,12 @@
+// here is the first time im using advanced data structure.
+// my intro to singly linked lists :)
+
+
+
+// what is a linked list?
+
+// a data structures that contain a head a tail and a length
+// a linked list has no indicies that just point to the next element
+//linked lists consists of nodes and 
+// each node has a value and a pointer that points to the next node 
+// or null
