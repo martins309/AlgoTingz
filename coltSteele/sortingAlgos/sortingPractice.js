eb0b59182658458676bedf28f1cb977cc7862110
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-10 22:10:19 -0400

    the comparison in the second loop was wrong the arugment is j+ 1 not i

--- coltSteele/sortingAlgos/sortingPractice.js
+++ coltSteele/sortingAlgos/sortingPractice.js
@@ -339,10 +339,10 @@ function bubbles(arr){
     function swap(arr, idx1, idx2){
         return [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
     }
-    for(let i = arr.length; i >0 ; i--){
-        for(let j = 0; j = i -1; j++){
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++){
             if(arr[j] > arr[j + 1]){
-                swap(arr, i, j)
+                swap(arr, j, j + 1)
             }
         }
     }
