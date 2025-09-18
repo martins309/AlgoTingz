184d06388839744d3a7f287d7cc7a64eb5162ec3
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-18 12:32:34 -0400

    now practicing the merge sort using the inital helper function

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -1,38 +1,76 @@
 // this is for the merge sort, quick sort and radix sort
 
-const js = require("@eslint/js")
-
 //the main ones are commited to memory and I know how and why they work :)
 
 
 
-function selectionSort(arr, comparator){
-    function swap(arr, idx1, idx2){
-        return [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+// function selectionSort(arr, comparator){
+//     function swap(arr, idx1, idx2){
+//         return [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+//     }
+
+//     if(typeof comparator !== 'function'){
+//         comparator = function(a, b){
+//             return a - b
+//         }
+//     }
+//     for(let i = 0; i < arr.length; i++){
+//         let min = i
+//         for(let j = i + 1; j < arr.length; j++){
+//             if(comparator(arr[min], arr[j]) > 0){
+//                 min = j
+//             }
+//         }
+//         if(i !== min){
+//             swap(arr, i, min)
+//         }
+//     }
+//     return arr
+// }
+
+// function comparator(a, b){
+//     return a > b ? 1 :
+//     a < b ? -1 : 0
+// }
+
+// console.log(selectionSort(['chicken', 'is', 'fum'], comparator))
+
+
+
+
+
+
+
+
+
+
+function mergeArr(arr1, arr2){
+    let results = []
+    let i = 0
+    let j = 0
+
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i])
+            i++
+        }
+
+        if(arr1[i] > arr2[j]){
+            results.push(arr2[j])
+            j++
+        }
+    }
+    
+    while(i < arr1.length){
+        results.push(arr[i])
+        i++
     }
 
-    if(typeof comparator !== 'function'){
-        comparator = function(a, b){
-            return a - b
-        }
+    while(j < arr2.length){
+        results.push(arr2[j])
+        j++
     }
-    for(let i = 0; i < arr.length; i++){
-        let min = i
-        for(let j = i + 1; j < arr.length; j++){
-            if(comparator(arr[min], arr[j]) > 0){
-                min = j
-            }
-        }
-        if(i !== min){
-            swap(arr, i, min)
-        }
-    }
-    return arr
+    return results
 }
 
-function comparator(a, b){
-    return a > b ? 1 :
-    a < b ? -1 : 0
-}
-
-console.log(selectionSort(['chicken', 'is', 'fum'], comparator))
+console.log(mergeArr([5,6,3], [1, 8, 9]))
