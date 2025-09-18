b8b4db4ac880eccd8d59bcf8aa83dcaae12b2711
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-18 12:14:04 -0400

    selection sort is correct, now for the merge sort

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -1,3 +1,38 @@
 // this is for the merge sort, quick sort and radix sort
 
+const js = require("@eslint/js")
+
 //the main ones are commited to memory and I know how and why they work :)
+
+
+
+function selectionSort(arr, comparator){
+    function swap(arr, idx1, idx2){
+        return [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+    }
+
+    if(typeof comparator !== 'function'){
+        comparator = function(a, b){
+            return a - b
+        }
+    }
+    for(let i = 0; i < arr.length; i++){
+        let min = i
+        for(let j = i + 1; j < arr.length; j++){
+            if(comparator(arr[min], arr[j]) > 0){
+                min = j
+            }
+        }
+        if(i !== min){
+            swap(arr, i, min)
+        }
+    }
+    return arr
+}
+
+function comparator(a, b){
+    return a > b ? 1 :
+    a < b ? -1 : 0
+}
+
+console.log(selectionSort(['chicken', 'is', 'fum'], comparator))
