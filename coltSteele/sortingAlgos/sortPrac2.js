dc3745a69fbb79e04563736a4dd41b575b0e3ccc
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-22 16:19:32 -0400

    wrote the merge arr portion and now to test

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -45,30 +45,32 @@
 
 
 function mergeArr(arr1, arr2){
-    let results = []
-    let i = 0
-    let j = 0
+     let results = []
+     let i = 0
+     let j = 0
 
-    while(i < arr1.length && j < arr2.length){
-        if(arr2[j] >= arr1[i]){
+     while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
             results.push(arr1[i])
             i++
         } else {
             results.push(arr2[j])
             j++
         }
-    }
-    
-    while(i < arr1.length){
+     }
+
+     while(i < arr1.length){
         results.push(arr1[i])
         i++
-    }
+     }
 
-    while(j < arr2.length){
+     while(j < arr2.length){
         results.push(arr2[j])
         j++
-    }
-    return results
+     }
+
+     return results
 }
 
-console.log(mergeArr([3,5,6], [1, 8, 9]))
+
+console.
