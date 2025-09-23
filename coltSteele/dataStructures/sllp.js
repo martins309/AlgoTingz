55840e0d30b323f8271e457c00af4acb9f12afc2
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-23 13:55:04 -0400

    we are now practicing sll and its subsequent methods

---
+++ coltSteele/dataStructures/sllp.js
@@ -0,0 +1,19 @@
+
+
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+
+    }
+}
+
+
+class singlyLinkedList{
+    constructor(){
+        this.head = null 
+        this.tail = null 
+        this.length = 0
+    }
+}
