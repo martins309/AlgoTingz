1f26e39a0b2aa947149fadfdfce1b0523fed2c32
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-23 13:44:00 -0400

    now to write the merge sort from memory

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -44,49 +44,107 @@
 
 
 
-function mergeArr(arr1, arr2){
-     let results = []
-     let i = 0
-     let j = 0
+// function mergeArr(arr1, arr2){
+//      let results = []
+//      let i = 0
+//      let j = 0
 
-     while(i < arr1.length && j < arr2.length){
-        if(arr2[j] > arr1[i]){
-            results.push(arr1[i])
-            i++
-        } else {
-            results.push(arr2[j])
-            j++
-        }
-     }
+//      while(i < arr1.length && j < arr2.length){
+//         if(arr2[j] > arr1[i]){
+//             results.push(arr1[i])
+//             i++
+//         } else {
+//             results.push(arr2[j])
+//             j++
+//         }
+//      }
 
-     while(i < arr1.length){
-        results.push(arr1[i])
-        i++
-     }
+//      while(i < arr1.length){
+//         results.push(arr1[i])
+//         i++
+//      }
 
-     while(j < arr2.length){
-        results.push(arr2[j])
-        j++
-     }
+//      while(j < arr2.length){
+//         results.push(arr2[j])
+//         j++
+//      }
 
-     return results
-}
-
-
-console.log(mergeArr([1,5,7], [3,6,9]))
+//      return results
+// }
 
 
 
 
-function mergeSort(arr){
-   if(arr.length <= 1) return arr
 
-   let mid = Math.floor(arr.length / 2)
-   let left = mergeSort(arr.slice(0, mid))
-   let right= mergeSort(arr.slice(mid))
+
+
+// function mergeSort(arr, comparator){
+//    if(arr.length <= 1) return arr
+
+//    let mid = Math.floor(arr.length / 2)
+//    let left = mergeSort(arr.slice(0, mid), comparator)
+//    let right= mergeSort(arr.slice(mid), comparator)
    
-   return mergeSort(left, right)
+//    return mergeArr(left, right, comparator)
+// }
+
+
+// const comparator = (a, b) => 
+//     a > b ? 1 :
+//     a < b ? -1 : 0
+
+// console.log(mergeSort(['zebra', 'anul', 'grepption', 'doofus'], comparator))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mergeArrays(arr1, arr2, comparator){
+   if(typeof comparator !== 'function'){
+      comparator = function(a,b){
+         return a - b
+      }
+   }
+   let results = []
+   let i = 0
+   let j = 0
+
+   while(i < arr1.length && j < arr2.length){
+      if(comparator(arr2[j], arr1[i]) > 0){
+         results.push(arr1[i])
+         i++
+      } else {
+         results.push(arr2[j])
+         j++
+      }
+   }
+
+   while(i < arr1.length){
+      results.push(arr1[i])
+      i++
+   }
+   
+   while(j < arr2.length){
+      results.push(arr2[j])
+      j++
+   }
+
+   return results
 }
 
-
-console.log(mergeSort('zebra', 'anul', 'grepption', 'doofus'))
+function comparator(a, b){
+   return a > b ? 1:
+   a < b ? -1 : 0
+}
+console.log(mergeArrays(['apples', 'dress', 'pie'], ['gays', 'rake', 'vibrator'], comparator))
