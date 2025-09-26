8eb6ef9d7c018cfacc0c03e807864526a6661e74
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-25 21:40:03 -0400

    wrote the merge arr function

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -110,15 +110,15 @@
 
 
 
-function mergeArrays(arr1, arr2, comparator){
+function mergeArr(arr1, arr2, comparator){
    if(typeof comparator !== 'function'){
-      comparator = function(a,b){
+      comparator = function(a, b) {
          return a - b
       }
    }
-   let results = []
    let i = 0
    let j = 0
+   let results = []
 
    while(i < arr1.length && j < arr2.length){
       if(comparator(arr2[j], arr1[i]) > 0){
@@ -134,31 +134,12 @@ function mergeArrays(arr1, arr2, comparator){
       results.push(arr1[i])
       i++
    }
-   
+
    while(j < arr2.length){
       results.push(arr2[j])
       j++
    }
-
-   return results
+      return results
 }
 
-
-
-
-function mergeSort(arr, comparator){
-   if(arr.length <= 1) return arr
-
-   let mid = Math.floor(arr.length / 2)
-   let left = mergeSort(arr.slice(0, mid), comparator)
-   let right = mergeSort(arr.slice(mid),comparator)
-
-   return mergeArrays(left, right, comparator)
-}
-
-function comparator(a, b){
-   return a > b ? 1:
-   a < b ? -1 : 0
-}
-
-console.log(mergeSort(['fuel', 'theez', 'nuts', 'with', 'praise'], comparator))
+console.log(mergeArr([1,4,7], [3,7,9], comparator))
