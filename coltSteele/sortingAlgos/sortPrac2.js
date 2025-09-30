cd6610f13751315814ceee5c6dd9b4eca733239b
Andrew Martins <andrew_omartins@yahoo.com>
2025-09-30 13:41:45 -0400

    bubbleGuts algo

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -205,31 +205,82 @@
 
 
 
-function mergeArr(arr1, arr2){
-   let results = []
-   let i = 0
-   let j = 0
+// function mergeArr(arr1, arr2){
+//    let results = []
+//    let i = 0
+//    let j = 0
 
-   while(arr1.length < i && arr2.length < j){
-      if(arr2[j] > arr1[i]){
-         results.push(arr1[i])
-         i++
-      } else {
-         results.push(arr2[j])
+//    while(i < arr1.length && j < arr2.length){
+//       if(arr2[j] > arr1[i]){
+//          results.push(arr1[i])
+//          i++
+//       } else {
+//          results.push(arr2[j])
+//          j++
+//       }
+//    }
+
+
+//    while(i < arr1.length ){
+//       results.push(arr1[i])
+//       i++
+//    }
+
+//    while(j < arr2.length){
+//       results.push(arr2[j])
+//       j++
+//    }
+//    return results
+// }
+
+
+// function mergeSort(arr){
+//    if(arr.length <= 1) return arr
+
+   
+//       let mid = Math.floor(arr.length / 2)
+//       let left = mergeSort(arr.slice(0, mid))
+//       let right = mergeSort(arr.slice(mid))
+//       return mergeArr(left, right)
+   
+  
+// }
+
+
+// console.log(mergeSort([3,7,6,1,4]))
+
+
+
+
+
+
+
+function bubbleGuts(arr, comparator){
+   let noSwap
+
+    if(typeof comparator !== 'function'){
+      comparator = function(a, b){
+         return a - b
       }
    }
-
-
-   while(arr1.length < i){
-      results.push(arr1[i])
-      i++
+   function swap(arr, idx1, idx2){
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
    }
-
-   while(arr2.length < j){
-      results.push(arr2[j])
-      j++
+   for(let i = arr.length; i > 0; i--){
+      for(let j = 0; j < i - 1; j++){
+         noSwap = false
+         if(comparator(arr[j], arr[j + 1]) > 0){
+            swap(arr, arr[j], arr[j + 1])
+            noSwap = true
+         }
+      }
    }
-   return results
+   return arr
 }
 
-console.log(mergeArr([1,5,7], [4,8,9]))
+function comparator(a, b) {
+   return a > b ? 1 :
+   a < b ? -1 : 0
+}
+
+console.log(bubbleGuts([300,467,2,1,5674,2,47,32,88,4], comparator))
