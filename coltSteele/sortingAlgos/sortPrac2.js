5b13a0b2472346d889fcaed907ac3c4ff5b88513
Andrew Martins <andrew_omartins@yahoo.com>
2025-10-06 17:42:04 -0400

    quick sort check

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -360,23 +360,36 @@
 
 
 
+function partition(arr, start = 0, end = arr.length - 1){
 
-function partition(arr, start = 0, end = arr.length -1) {
    function swap(arr, idx1, idx2){
-      [arr[idx1], ar[idx2]] = [arr[idx2], arr[idx1]]
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
    }
 
    let pivot = arr[start]
    let swapIdx = start
 
-   for(let i = start + 1; i <= end; i++) {
+   for(let i = start + 1; i <= end; i++){
       if(pivot > arr[i]){
          swapIdx++
          swap(arr, swapIdx, i)
       }
    }
-
    swap(arr, start, swapIdx)
-
    return swapIdx
 }
+
+
+
+function quickSort(arr, left = 0, right = arr.length - 1){
+
+   if(left < right) {
+      let pivotIdx = partition(arr, left, right)
+      quickSort(arr, left, pivotIdx - 1)
+      quickSort(arr, pivotIdx + 1, right)
+   }
+
+   return arr
+}
+
+console.log(quickSort([4,5,6,7,8,1,2,3]))
