805ed2678ef882259a7c63b05b6c1c9f88f192c7
Andrew Martins <andrew_omartins@yahoo.com>
2025-10-05 20:32:09 -0400

    quick sort helper

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -328,31 +328,55 @@
 //futatabi yare!!
 
 
-function selectionSort(arr, comparator){
-   if(typeof comparator !== 'function'){
-      comparator = function(a, b){
-         return a - b
+// function selectionSort(arr, comparator){
+//    if(typeof comparator !== 'function'){
+//       comparator = function(a, b){
+//          return a - b
+//       }
+//    }
+//    function swap(arr, idx1, idx2){
+//       [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+//    }
+
+//    for(let i = 0; i < arr.length; i++){
+//       let min = i
+//       for(let j = i + 1; j < arr.length; j++){
+//          if(comparator(arr[min], arr[j]) > 0){
+//             min = j
+//          }
+//       }
+//       if(i !== min) swap(arr, i, min)
+//    }
+//    return arr
+// }
+
+// function comparator(a, b){
+//    return a > b ? 1 :
+//    a < b ? -1 : 0
+// }
+
+// console.table(selectionSort(['chicken', 'butt', 'pork', 'belly', 'monkey', 'brains'], comparator))
+
+
+
+
+
+function partition(arr, start = 0, end = arr.length -1) {
+   const swap(arr, idx1, idx2){
+      [arr[idx1], ar[idx2]] = [arr[idx2], arr[idx1]]
+   }
+
+   let pivot = arr[start]
+   let swapIdx = start
+
+   for(let i = start + 1; i <= end; i++) {
+      if(pivor > arr[i]){
+         swapIdx++
+         swap(arr, swapIdx, i)
       }
    }
-   function swap(arr, idx1, idx2){
-      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-   }
 
-   for(let i = 0; i < arr.length; i++){
-      let min = i
-      for(let j = i + 1; j < arr.length; j++){
-         if(comparator(arr[min], arr[j]) > 0){
-            min = j
-         }
-      }
-      if(i !== min) swap(arr, i, min)
-   }
-   return arr
+   swap(arr, start, swapIdx)
+
+   return swapIdx
 }
-
-function comparator(a, b){
-   return a > b ? 1 :
-   a < b ? -1 : 0
-}
-
-console.table(selectionSort(['chicken', 'butt', 'pork', 'belly', 'monkey', 'brains'], comparator))
