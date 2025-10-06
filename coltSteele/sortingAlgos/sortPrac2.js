5b807e6468df7f55f905b80d392093b163b4656d
Andrew Martins <andrew_omartins@yahoo.com>
2025-10-06 17:47:00 -0400

    wrote it from scartch and i think that it is flawless

--- coltSteele/sortingAlgos/sortPrac2.js
+++ coltSteele/sortingAlgos/sortPrac2.js
@@ -360,36 +360,41 @@
 
 
 
-function partition(arr, start = 0, end = arr.length - 1){
+// write the quick sort from scratch
+
+
+
+
+function partition(arr, start = 0, end = arr.length - 1) {
 
    function swap(arr, idx1, idx2){
-      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
    }
 
    let pivot = arr[start]
    let swapIdx = start
 
-   for(let i = start + 1; i <= end; i++){
+   for(let i = start + 1; i <= end; i++) {
       if(pivot > arr[i]){
          swapIdx++
          swap(arr, swapIdx, i)
       }
    }
+
    swap(arr, start, swapIdx)
+
    return swapIdx
 }
 
+function quickSort(arr, left = 0, right = arr.length - 1) {
 
+   if(left < right){
 
-function quickSort(arr, left = 0, right = arr.length - 1){
-
-   if(left < right) {
       let pivotIdx = partition(arr, left, right)
-      quickSort(arr, left, pivotIdx - 1)
-      quickSort(arr, pivotIdx + 1, right)
+
+      quickSort(arr, start, pivotIdx - 1)
+      quickSort(arr, pivotIdx + 1, end)
    }
 
    return arr
 }
-
-console.log(quickSort([4,5,6,7,8,1,2,3]))
