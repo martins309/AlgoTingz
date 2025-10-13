ba5b90c044b57db0af1bc979fb6dc19c0b6f34f6
Andrew Martins <andrew_omartins@yahoo.com>
2025-10-13 12:08:48 -0400

    i think something is wrong but lets check

--- coltSteele/sortingAlgos/finalSP.js
+++ coltSteele/sortingAlgos/finalSP.js
@@ -12,9 +12,16 @@ function insertionSort(arr, comparator){
         let currentVal = arr[i]
         let j
         for(j = i - 1; j >= 0 && currentVal > arr[j]; j--){
-            arr[j + 1] = j
+            arr[j + 1] = arr[j]
         }
         arr[j + 1] = currentVal
     }
     return arr
 }
+
+function comparator(a, b){
+    return a > b ? 1:
+    a < b ? -1 : 0
+}
+
+console.table(insertionSort([3,4,5,1,7,9,8,6], comparator))
