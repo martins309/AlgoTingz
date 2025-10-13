9a376437625d6e726a6180f5f61b8bd71d34ccd5
Andrew Martins <andrew_omartins@yahoo.com>
2025-10-13 12:07:08 -0400

    insertion sort prac

--- coltSteele/sortingAlgos/finalSP.js
+++ coltSteele/sortingAlgos/finalSP.js
@@ -2,3 +2,19 @@
 
 // start with the methods obvi and then go from there
 
+function insertionSort(arr, comparator){
+    if(typeof comparator !== 'function'){
+        comparator = function(a, b){
+            return a - b
+        }
+    }
+    for(let i = 1; i < arr.length; i++){
+        let currentVal = arr[i]
+        let j
+        for(j = i - 1; j >= 0 && currentVal > arr[j]; j--){
+            arr[j + 1] = j
+        }
+        arr[j + 1] = currentVal
+    }
+    return arr
+}
