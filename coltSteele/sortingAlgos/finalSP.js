818e0291150467f7ddb4176fe98ac2826f4c201b
Andrew Martins <andrew_omartins@yahoo.com>
2025-10-14 08:29:54 -0400

    merging the array ting

--- coltSteele/sortingAlgos/finalSP.js
+++ coltSteele/sortingAlgos/finalSP.js
@@ -61,3 +61,40 @@
 // }
 
 // console.table(quickSort([5,6,7,8,9,1,2,3,4,0]))
+
+
+
+
+
+
+
+function mergeArr(arr1, arr2){
+
+    let i = 0
+    let j = 0
+    let results = []
+
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i])
+            i++
+        } else {
+            results.push(arr2[j])
+            j++
+        }
+    }
+
+    while(i < arr1.length){
+        results.push(arr1[i])
+        i++
+    }
+
+    while(j < arr2.length){
+        results.push(arr2[j])
+        j++
+    }
+
+    return results
+}
+
+console.log(mergeArr([1,5,9], [2,4,8]))
