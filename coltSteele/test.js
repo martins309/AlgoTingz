7ca767ce53e91044c489856a05acd6db42cd1a0f
Andrew Martins <andrew_omartins@yahoo.com>
2025-11-06 21:33:51 -0500

    look at this

--- coltSteele/test.js
+++ coltSteele/test.js
@@ -9,7 +9,6 @@ function getDigit(num, i){
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
 }
 
-
 function digitCount(num){
     if(num === 0) return 1
     return Math.floor(Math.log10(Math.abs(num))) + 1
@@ -17,24 +16,23 @@ function digitCount(num){
 
 function mostDigits(nums){
     let max = 0
-    for (let i = 0; i < nums.length; i++){
-        max = Math.max(max, digitCount(nums[i]))
+    for(let i = 0; i < nums.length; i++ ){
+         max = Math.max(max, digitCount(nums[i]))
     }
-
     return max
 }
 
-
 function radixSort(nums){
     let count = mostDigits(nums)
-    for (let k = 0; k < count; k++){
+    for(let k = 0; k < count; k++){
         let buckets = Array.from({ length: 10}, () => [])
         for(let i = 0; i < nums.length; i++){
             buckets[getDigit(nums[i], k)].push(nums[i])
         }
-       nums = [].concat(...buckets)
+        nums = [].concat(...buckets)
     }
     return nums
 }
 
-console.table(radixSort([902, 4, 7, 408, 29, 9637, 1556, 3556, 8157, 4386, 86, 593]))
+console.table()
+       
