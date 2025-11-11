fb90a94ce856feaa6d69746fd96633fe7b5d9473
Andrew Martins <andrew_omartins@yahoo.com>
2025-11-11 10:50:49 -0500

    wrote the methods for radix again, bout to test then write the main function

--- coltSteele/test.js
+++ coltSteele/test.js
@@ -10,29 +10,17 @@ function getDigit(num, i){
 }
 
 function digitCount(num){
-    if(num === 0) return 1
-    return Math.floor(Math.log10(Math.abs(num))) + 1
+    if(num === 1) return 1
+    return(Math.floor(Math.log10(Math.abs(num)))) + 1
 }
 
 function mostDigits(nums){
     let max = 0
-    for(let i = 0; i < nums.length; i++ ){
-         max = Math.max(max, digitCount(nums[i]))
+    for(let i = max; i < nums.length; i++){
+        max = Math.max(max, digitCount(nums[i]))
     }
     return max
 }
 
-function radixSort(nums){
-    let count = mostDigits(nums)
-    for(let k = 0; k < count; k++){
-        let buckets = Array.from({ length: 10}, () => [])
-        for(let i = 0; i < nums.length; i++){
-            buckets[getDigit(nums[i], k)].push(nums[i])
-        }
-        nums = [].concat(...buckets)
-    }
-    return nums
-}
-
-console.table()
+console.log(mostDigits([1,22,3,66545,47898465,1235]))
        
