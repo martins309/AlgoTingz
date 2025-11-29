cb2b8a6aa78fed40709d9c2cfb58d581615e1d27
Andrew Martins <andrew_omartins@yahoo.com>
2025-11-28 22:24:34 -0500

    flawless victory

--- coltSteele/test.js
+++ coltSteele/test.js
@@ -154,8 +154,10 @@ function mergeArr(arr1, arr2){
     while(i < arr1.length && j < arr2.length){
         if(arr2[j] > arr1[i]){
             results.push(arr1[i])
+            i++
         } else {
             results.push(arr2[j])
+            j++
         }
     }
 
@@ -171,4 +173,14 @@ function mergeArr(arr1, arr2){
 }
 
 
-console.log(mergeArr([1,4,5], [7,8,9]))
+function mergeSort(arr){
+    if(arr.length === 1) return arr
+    let mid = Math.floor(arr.length / 2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+
+    return mergeArr(left, right)
+}
+
+
+console.table(mergeSort([6,1,3,2,4,7,9,2,5]))
