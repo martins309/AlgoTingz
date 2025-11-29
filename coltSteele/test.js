8f70df52b9f244c1ca7ec2f24b69f61d4eb2a4ef
Andrew Martins <andrew_omartins@yahoo.com>
2025-11-28 21:54:15 -0500

    i give 94. We needed to add one because you dont need to compare the first value, go all the way to the end in the comparison portion of the loop, and in the swap functions we are swapping the indices not the acutal value, in the main function just add the partition var in the if statement, other than that that was solid

--- coltSteele/test.js
+++ coltSteele/test.js
@@ -5,73 +5,73 @@
 
 
 
-function getDigit(num, i){
-    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
-}
+// function getDigit(num, i){
+//     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
+// }
 
-function digitCount(num){
-    if(num === 1) return 1
-    return(Math.floor(Math.log10(Math.abs(num)))) + 1
-}
+// function digitCount(num){
+//     if(num === 1) return 1
+//     return(Math.floor(Math.log10(Math.abs(num)))) + 1
+// }
 
-function mostDigits(nums){
-    let max = 0
-    for(let i = max; i < nums.length; i++){
-        max = Math.max(max, digitCount(nums[i]))
-    }
-    return max
-}
+// function mostDigits(nums){
+//     let max = 0
+//     for(let i = max; i < nums.length; i++){
+//         max = Math.max(max, digitCount(nums[i]))
+//     }
+//     return max
+// }
 
-function radixSort(nums){
-    let count = mostDigits(nums)
-    for(let k = 0; k < count; k++){
-        let buckets = Array.from({ length: 10}, () => [])
-        for(let i = 0; i < nums.length; i++){
-            buckets[getDigit(nums[i], k)].push(nums[i])
-        }
-        nums = [].concat(...buckets)
-    }
-    return nums
-}       
+// function radixSort(nums){
+//     let count = mostDigits(nums)
+//     for(let k = 0; k < count; k++){
+//         let buckets = Array.from({ length: 10}, () => [])
+//         for(let i = 0; i < nums.length; i++){
+//             buckets[getDigit(nums[i], k)].push(nums[i])
+//         }
+//         nums = [].concat(...buckets)
+//     }
+//     return nums
+// }       
 
-console.table(radixSort([902, 4, 7, 408, 29, 9637, 1556, 3556, 8157, 4386, 86, 593]))
-
-
+// console.table(radixSort([902, 4, 7, 408, 29, 9637, 1556, 3556, 8157, 4386, 86, 593]))
 
 
 
-// time for the bubbles to sort
 
 
-function bubbleSort(arr, comparator){
-    let noSwap
-    if(typeof comparator !== 'function'){
-        comparator = function(a, b){
-            return a - b
-        }
-    }
-    function swap(arr, idx1, idx2){
-        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-    }
+// // time for the bubbles to sort
 
-    for(let i = arr.length; i > 0; i--){
-        noSwap = true
-        for(let j = 0; j < i - 1; j++){
-            if(comparator(arr[j], arr[j + 1]) > 0){
-                swap(arr, j, j + 1)
-                noSwap = false
-            }
-        }
-        if(noSwap) break
-    }
-    return arr
-}
 
-function comparator(a, b){
-    return a > b ? 1 : a < b ? -1 : 0
-}
+// function bubbleSort(arr, comparator){
+//     let noSwap
+//     if(typeof comparator !== 'function'){
+//         comparator = function(a, b){
+//             return a - b
+//         }
+//     }
+//     function swap(arr, idx1, idx2){
+//         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+//     }
 
-console.table(bubbleSort([7,5,3,2,6,4,99,5,65,12,63,4,6,9]))
+//     for(let i = arr.length; i > 0; i--){
+//         noSwap = true
+//         for(let j = 0; j < i - 1; j++){
+//             if(comparator(arr[j], arr[j + 1]) > 0){
+//                 swap(arr, j, j + 1)
+//                 noSwap = false
+//             }
+//         }
+//         if(noSwap) break
+//     }
+//     return arr
+// }
+
+// function comparator(a, b){
+//     return a > b ? 1 : a < b ? -1 : 0
+// }
+
+// console.table(bubbleSort([7,5,3,2,6,4,99,5,65,12,63,4,6,9]))
 
 
 //this shits in my brain son
@@ -88,28 +88,30 @@ function partition(arr, start = 0, end = arr.length - 1){
         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
     }
 
-    let pivotPoint = arr[start]
+    let pivot = arr[start]
 
     let swapIdx = start
 
-    for(let i = start; i < end; i++){
+    for(let i = start + 1; i <= end; i++){
         if(pivot > arr[i]){
             swapIdx++
             swap(arr, swapIdx, i)
         }
-        swap(arr, start, pivotPoint)
+        
     }
+    swap(arr, start, swapIdx)
     return swapIdx
 }
 
 
 function quickSort(arr, left = 0, right = arr.length - 1){
     
-    let pivotIdx = partition(arr, left, right)
+    
 
     if(left < right){
-        quickSort(arr, left, pivotIdx -1)
-        quickSort(arr, pivotIdx + 1, end)
+        let pivotIdx = partition(arr, left, right)
+        quickSort(arr, left, pivotIdx - 1)
+        quickSort(arr, pivotIdx + 1, right)
     }
     return arr
 }
