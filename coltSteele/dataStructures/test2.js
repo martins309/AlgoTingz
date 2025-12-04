45f07cc938f776f62df87b26f98a999e3565598d
Andrew Martins <andrew_omartins@yahoo.com>
2025-12-04 08:54:22 -0500

    okay that went better than I thought. I gotta stop doubting myself

--- coltSteele/dataStructures/test2.js
+++ coltSteele/dataStructures/test2.js
@@ -1,18 +1,16 @@
 // so here we are going to study the singly linked list ting
 
 
-
-
 class Node {
     constructor(val){
-        this.val = val 
-        this.next = null 
+        this.val = null
+        this.next = null
     }
 }
 
 class SinglyLinkedList {
     constructor(){
-        this.head = null 
+        this.head = null
         this.tail = null
         this.length = 0
     }
@@ -22,20 +20,16 @@ class SinglyLinkedList {
         if(!this.head){
             this.head = newNode
             this.tail = this.head
-        } else{
+        }else {
             this.tail.next = newNode
-            this.tail = newNode
+            this.tail = this.next
         }
-        this.length++
         return this
+        this.length++
     }
-
-
-
-
-
-
 }
+
+
 
 
 let list = new SinglyLinkedList()
