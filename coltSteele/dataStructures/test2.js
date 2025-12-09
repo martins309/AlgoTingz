110fdb66f213d1585e4183d517e580aa96be7653
Andrew Martins <andrew_omartins@yahoo.com>
2025-12-09 13:18:45 -0500

    i think this is flawless as well

--- coltSteele/dataStructures/test2.js
+++ coltSteele/dataStructures/test2.js
@@ -5,28 +5,63 @@
 //insertion sort
 
 
-function insertionSort(arr, comparator){
-    if(typeof comparator !== 'function'){
-        comparator = function(a, b){
-            return a - b
-        }
-    }
-    for(let i = 1; i < arr.length; i++){
-        let currentVal = arr[i]
-        let j = i - 1
-        while(j >= 0 && comparator(arr[j], currentVal) > 0){
-            arr[j + 1] = arr[j]
-            j--
-        }
+// function insertionSort(arr, comparator){
+//     if(typeof comparator !== 'function'){
+//         comparator = function(a, b){
+//             return a - b
+//         }
+//     }
+//     for(let i = 1; i < arr.length; i++){
+//         let currentVal = arr[i]
+//         let j = i - 1
+//         while(j >= 0 && comparator(arr[j], currentVal) > 0){
+//             arr[j + 1] = arr[j]
+//             j--
+//         }
         
-        arr[j + 1] = currentVal
+//         arr[j + 1] = currentVal
 
+//     }
+//     return arr
+// }
+
+// function comparator(a, b){
+//     return a > b ? 1 : a < b ? -1 : 0
+// }
+
+// console.table(insertionSort([4,12,6,78,5,34]))
+
+
+
+function partition(arr, start = 0, end = arr.length - 1){
+    function swap(arr, idx1, idx2){
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
     }
+
+    let pivot = arr[start]
+    let swapIdx = start
+
+    for(let i = start + 1; i <= end; i++){
+        if(pivot > arr[i]){
+            swapIdx++
+            swap(arr, swapIdx, i)
+        }
+        swap(arr, start, swapIdx)
+    }
+    return swapIdx
+}
+
+
+function quickSort(arr, left = 0, right = arr.length - 1){
+
+    if(left < right){
+        let pivotIdx = partition(arr, left, right)
+
+        quickSort(arr, left, pivotIdx -1)
+        quickSort(arr, pivotIdx + 1, right)
+    }
+
     return arr
 }
 
-function comparator(a, b){
-    return a > b ? 1 : a < b ? -1 : 0
-}
-
-console.table(insertionSort([4,12,6,78,5,34]))
+console.table(quickSort([4,5,6,1,2,3,.9,-1]))
