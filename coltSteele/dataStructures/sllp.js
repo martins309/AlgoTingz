88122fd716915092d20956c3982ca793f7f06c7f
Andrew Martins <andrew_omartins@yahoo.com>
2025-12-09 13:35:53 -0500

    check it

--- coltSteele/dataStructures/sllp.js
+++ coltSteele/dataStructures/sllp.js
@@ -30,4 +30,37 @@ class singlyLinkedList{
         this.length++ 
         return this
     }
+
+    pop(){
+        if(!this.head) return undefined
+
+        let current = this.head
+        let newTail = current
+
+        while(current.next){
+            newTail = current 
+            current = current.next
+        }
+
+        this.tail = newTail
+        this.tail.next = null
+        this.length-- 
+
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return current 
+    }
 }
+
+
+
+let list = singlyLinkedList()
+
+list.push("hello")
+list.push("how")
+list.push("are")
+list.push("you")
+
+console.log(list)
