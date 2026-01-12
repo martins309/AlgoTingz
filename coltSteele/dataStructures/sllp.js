588a728ec33a399137fb5d92f798461393e0e2e8
Andrew Martins <andrew_omartins@yahoo.com>
2026-01-12 13:50:39 -0500

    do it again till we get all the methods right

--- coltSteele/dataStructures/sllp.js
+++ coltSteele/dataStructures/sllp.js
@@ -4,7 +4,7 @@
 class Node{
     constructor(val){
         this.val = val
-        this.next = null
+        this.next = next
     }
 }
 
@@ -20,66 +20,12 @@ class SinglyLinkedList{
         if(!this.head){
             this.head = newNode
             this.tail = this.head
-        } else {
+        }else {
             this.tail.next = newNode
             this.tail = newNode
         }
         this.length++
         return this
-    }
-    pop(){
-        if(!this.head) return undefined
-        let current = this.head
-        let newTail = current
-
-        while(current.next){
-            newTail = current
-            current = current.next
-        }
-        this.length--
-        this.tail = newTail
-        this.tail.next = null
-        if(this.length === 0){
-            this.head = null
-            this.tail = null
-        }
-        return current
-        
-    }
-    shift(){
-        if(!this.head) return undefined
-        let current = this.head 
-        this.head = current
-        this.length--
-        if(this.length === 0){
-            this.tail = null
-        }
-        return current
-    }
-
-    unshift(val){
-        let newNode = new Node(val)
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        } else {
-            newNode.next = newNode
-            this.head = newNode
-        }
-        this.length++
-        return this
-    }
-    get(idx, val){
-        if(idx < 0 || idx >= this.length)return null
-
-        
-    }
-
-    set(val){
-        let node = this.get(val)
-        if(node){
-            
-        }
     }
 }
 
