4b02058c6b108a7f6ff56968e2b20ad37dae479d
Andrew Martins <andrew_omartins@yahoo.com>
2026-01-12 13:35:02 -0500

    i got to the get method and then everything started to fall apart lol

--- coltSteele/dataStructures/sllp.js
+++ coltSteele/dataStructures/sllp.js
@@ -17,79 +17,69 @@ class SinglyLinkedList{
 
     push(val){
         let newNode = new Node(val)
-
         if(!this.head){
             this.head = newNode
             this.tail = this.head
-        } else{
+        } else {
             this.tail.next = newNode
             this.tail = newNode
         }
         this.length++
         return this
     }
-
     pop(){
-        let current = this.head 
+        if(!this.head) return undefined
+        let current = this.head
         let newTail = current
-
-
-        if(!this.head)return undefined
 
         while(current.next){
             newTail = current
             current = current.next
         }
+        this.length--
         this.tail = newTail
         this.tail.next = null
-        this.length--
         if(this.length === 0){
             this.head = null
             this.tail = null
         }
         return current
+        
     }
-
     shift(){
-        let current = this.head
-        this.head = current.next
+        if(!this.head) return undefined
+        let current = this.head 
+        this.head = current
         this.length--
         if(this.length === 0){
             this.tail = null
         }
-        return this
-    }
-
-    unshift(val){
-        let newNode = newNode(val)
-
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        } else{
-            newNode.next = this.head
-            this.head = newNode
-        }
-    }
-
-    get(idx){
-        if(idx < 0 || idx >= this.length) return null
-        let count = 0
-        let current = this.head
-        while(count !== idx){
-            current = current.next  
-            count++
-        }
         return current
     }
 
-    set(idx, val){
-        let node = this.get(idx)
-        if(node){
-            node.val = val
-            return true
+    unshift(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            newNode.next = newNode
+            this.head = newNode
         }
-        return false
+        this.length++
+        return this
+    }
+    get(idx, val){
+        if(idx < 0 || idx >= this.length)return null
+
+        
+    }
+
+    set(val){
+        let node = this.get(val)
+        if(node){
+            
+        }
     }
 }
 
@@ -99,9 +89,10 @@ let list = new SinglyLinkedList()
 list.push("I'm")
 list.push("rich")
 list.push("as fuck")
+list.unshift("baby")
 
 
 
 
 
-console.log(list.get(2))
+console.log(list)
