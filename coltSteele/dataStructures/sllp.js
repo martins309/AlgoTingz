24b52206c7ac4eed13cf3bfc6979aa0ea4513cc3
Andrew Martins <andrew_omartins@yahoo.com>
2026-01-15 10:57:34 -0500

    we will review removed, insert and add reverse tomorrow

--- coltSteele/dataStructures/sllp.js
+++ coltSteele/dataStructures/sllp.js
@@ -27,4 +27,110 @@ class SinglyLinkedList{
         this.length++
         return this
     }
+
+    pop(){
+        if(!this.head) return undefined
+
+        let current = this.head
+        let newTail = current
+
+        while(current.next){
+            newTail = current
+            current = current.next
+        }
+        this.tail = newTail
+        this.tail.next = null
+        this.length--
+        
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return current
+    }
+
+    shift(){
+
+        let current = this.head
+        this.head = current
+        this.length--
+
+        if(this.length === 0){
+            this.tail = null
+        }
+        return current
+    }
+
+    unshift(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            newNode.next = newNode
+            this.head = newNode
+        }
+        this.length++ 
+        return this
+    }
+
+    get(idx){
+        if(idx < 0 || idx >= this.length) return null
+
+        let count = 0
+        let current = this.head
+        while(count !== idx){
+            current = current.next
+            count++
+        }
+        return current
+    }
+
+    set(val){
+        let node = this.get(idx)
+
+        if(node){
+            node.val = val
+            return true
+        }
+        return false
+    }
+
+    insert(idx, val){
+        let newNode = new Node(val)
+        if(idx < 0 || idx > this.length) return false
+        if(idx === this.length) return !!this.push(val)
+        if(idx === 0) return !!this.unshift(val)
+        
+        let prev = this.get(idx - 1)
+        let temp = prev.next
+        prev.next = newNode
+        newNode.next = temp
+
+        return true
+    }
+
+    remove(idx, val){
+        let newNode = new Node(val)
+        if(idx < 0 || idx > this.length) return null
+        if(idx === this.length -1) return this.pop()
+        if(idx === 0) return this.shift()
+        let prev = this.get(val)
+        let removed = prev.next
+        prev.next = removed.next
+
+        this.length--
+        return removed
+    }
 }
+
+let list = new SinglyLinkedList()
+
+
+list.push("ware")
+list.push("wa")
+list.push("kami da..")
+
+
+
+console.log(list.remove(2,"kami da"))
