f561a773423873e973331445831b49e1b69f93ad
Andrew Martins <andrew_omartins@yahoo.com>
2026-01-15 10:27:58 -0500

    push ting

--- coltSteele/dataStructures/sllp.js
+++ coltSteele/dataStructures/sllp.js
@@ -1,11 +1,11 @@
 
-
 class Node{
     constructor(val){
         this.val = val
         this.next = null
     }
 }
+
 
 class SinglyLinkedList{
     constructor(){
@@ -27,109 +27,4 @@ class SinglyLinkedList{
         this.length++
         return this
     }
-
-    pop(){
-        if(!this.head) return undefined
-        let current = this.head
-        let newTail = current
-
-        while(current.next){
-            newTail = current
-            current = current.next
-        }
-        this.tail = newTail
-        this.tail.next = null
-        this.length--
-
-        if(this.length === 0){
-            this.head = null
-            this.tail = null
-        }
-        return current
-    }
-
-    shift(){
-        let current = this.head
-        this.head = current
-        this.length--
-        if(this.length === 0){
-            this.tail = null
-        }
-        return current
-    }
-
-    unshift(val){
-        let newNode = new Node(val)
-
-        if(!this.head){
-            this.head = newNode
-            this.tail = newNode
-        } else {
-            newNode.next = newNode
-            this.head = newNode
-        }
-        this.length++
-        return this
-    }
-
-    get(idx){
-        if(idx < 0 || idx >= this.length) return null
-        let count = 0
-        let current = this.head
-        while(count !== idx){
-            current = current.next
-            count++
-        }
-        return current
-    }
-
-    set(idx, val){
-        let node = this.get(idx)
-        if(node){
-            node.val = val
-            return true
-        }
-        return false
-    }
-    insert(idx, val){
-        let newNode = new Node(val)
-
-        if(idx < 0 || idx > this.length) return false
-        if(idx === this.length) return !!this.push(val)
-        if(idx === 0) return !!this.unshift(val)
-        let prev = this.get(idx - 1)
-        let temp = prev.next 
-        prev.next = newNode
-        newNode.next = temp
-
-        this.length++
-        return true
-    }
-    remove(idx){
-        if(idx < 0 || idx > this.length) return false
-        if(idx === this.length -1) return this.pop()
-        if(idx === 0) return this.shift()
-        
-        let prev = this.get(idx - 1)
-        let removed = prev.next
-        prev.next = removed.next
-
-        this.length--
-        return removed
-    }
-    
-
-
 }
-
-
-
-const list = new SinglyLinkedList()
-
-list.push("money")
-list.push("on my")
-list.push("mind")
-list.set(0, "bitxhes")
-
-
-console.log(list)
