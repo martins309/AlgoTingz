d4496860fcaabbe5d820a62db92800dcc9132526
Andrew Martins <andrew_omartins@yahoo.com>
2026-01-21 13:05:32 -0500

    you cant stop me

--- coltSteele/dataStructures/sllp.js
+++ coltSteele/dataStructures/sllp.js
@@ -1,4 +1,3 @@
-
 class Node{
     constructor(val){
         this.val = val
@@ -16,7 +15,7 @@ class SinglyLinkedList{
     push(val){
         let newNode = new Node(val)
         if(!this.head){
-            this.head = newNode
+            this.head =newNode
             this.tail = this.head
         } else {
             this.tail.next = newNode
@@ -25,79 +24,15 @@ class SinglyLinkedList{
         this.length++
         return this
     }
-
-    pop(){
-        if(!this.head) return undefined
-
-        let current = this.head
-        let newTail = current
-
-        while(current.next){
-            newTail = current
-            current = current.next
-        } 
-        this.tail = newTail
-        this.tail.next = null
-        this.length--
-        if(this.length === 0){
-            this.head === null
-            this.tail === null
-        }
-        return current
-    }
-    shift(){
-
-        let current = this.head
-        this.head = current
-        this.length--
-        if(this.length === 0){
-            this.tail === null
-        }
-        return current
-    }
-    unshift(val){
-        let newNode = new Node(val)
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        } else {
-            newNode.next = newNode
-            this.head = newNode
-        }
-        this.length++
-        return this
-    }
-    get(idx){
-        if(idx < 0 || idx >= this.length) return null
-        let count = 0
-        let current = this.head
-        while(count !== idx){
-            current = current.next
-            count++
-        }
-        return current
-
-    }
-
-    set(idx, val){
-        let node = this.get(idx)
-        if(node){
-            node.val = val
-            return true
-        }
-        return false
-    }
 }
 
 
 
 let list = new SinglyLinkedList()
 
-list.push("im")
-list.push("rich")
-list.push("as")
-list.push("fuck!!")
+list.push("i")
+list.push("like to")
+list.push("fuck")
 
 
-console.log(list.set(0, "fagget"))
-console.log(list.get(0))
+console.log(list)
