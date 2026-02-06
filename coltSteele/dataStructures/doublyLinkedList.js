a352670bfba44c1b9e3ccc9127f4ed80692072c7
Andrew Martins <andrew_omartins@yahoo.com>
2026-02-05 19:54:21 -0500

    we got this shit

--- coltSteele/dataStructures/doublyLinkedList.js
+++ coltSteele/dataStructures/doublyLinkedList.js
@@ -22,4 +22,39 @@ class DoublyLinkedList{
         this.tail = null
         this.length = 0
     }
+    //create a new node with a val passed to the function
+    //if head is null, set the head and tail property to be the new node
+    //if not set the next property on the the tail to be that node
+    //set the prev property of the newly created node to tbe the tail
+    // set the tail to be the new node
+    //increment the length
+    // return the entire list
+
+
+    push(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
 }
+
+
+let list = new DoublyLinkedList()
+
+
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
+
+
+console.log(list)
