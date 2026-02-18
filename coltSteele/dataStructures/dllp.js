64c97774415106a93438ab3dd9d572c426b8df37
Andrew Martins <andrew_omartins@yahoo.com>
2026-02-18 12:07:16 -0500

    remember that we are changing the assisngments and not the nodes themselves

--- coltSteele/dataStructures/dllp.js
+++ coltSteele/dataStructures/dllp.js
@@ -16,54 +16,55 @@ class DoublyLinkedList{
         this.length = 0
     }
 
-    push(val){
-        let newNode = new Node(val)
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        } else {
-            this.tail.next = newNode
-            newNode.prev = this.tail
-            this.tail = newNode
-        }
-        this.length++
-        return this
+   push(val){
+    let newNode = new Node(val)
+
+    if(!this.head){
+        this.head = newNode
+        this.tail = this.head
+    } else {
+        this.tail.next = newNode
+        newNode.prev =  this.tail
+        this.tail = newNode
     }
+    this.length++
+    return this
 
-    pop(){
-        if(!this.length === 0) return undefined
+   }
 
-        let currentTail = this.tail
-        if(this.length === 1){
-            this.head = null
-            this.tail = null
-        } else {
-            this.tail = currentTail.prev
-            this.tail.next = null
-            currentTail.prev = null
-        }
+   pop(){
+    if(!this.head) return undefined
+    
+    let currentTail = this.tail
 
-        this.length--
-        return currentTail
+    if(this.length === 1){
+        this.head = null
+        this.tail = null
+    } else {
+        this.tail = currentTail.prev
+        this.tail.next = null
+        currentTail.prev = null
     }
+    this.length--
+    return currentTail
+   }
 
-    shift(){
-        if(!this.head) return undefined
+   shift(){
+    if(!this.head) return undefined
 
-        let oldHead = this.head
+    let oldHead = this.head
 
-        if(this.length === 0){
-            this.head = null
-            this.tail = null
-        } else {
-            this.head = oldHead.next
-            this.head.prev = null
-            oldHead.next = null
-        }
-        this.length--
-        return oldHead
-        
+    if(this.length === 1){
+        this.head = null
+        this.tail = null
+    } else {
+        this.head = oldHead.next 
+        this.head.prev = null
+        oldHead.next = null
     }
+    this.length--
+    return oldHead
+   }
 }
 
 
@@ -78,7 +79,7 @@ list.push('hey')
 list.push('now')
 list.push('you\'re a' )
 list.push('rock star')
-list.shift()
+
 
 
 console.log(list)
