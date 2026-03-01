2f49c8998c6e863fdf5c42f8a6f758851b732e60
Andrew Martins <andrew_omartins@yahoo.com>
2026-03-01 18:33:45 -0500

    this is to test how i think without looking at it. Think it through cuz it acutally makes sense

---
+++ coltSteele/dataStructures/t3.js
@@ -0,0 +1,37 @@
+//think it through
+
+
+
+
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+        this.prev = null
+    }
+}
+
+
+
+class DLL{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+}
