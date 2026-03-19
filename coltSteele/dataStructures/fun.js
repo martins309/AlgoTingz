90af1119c0cd8f4e504ffd10c863f968ccb5e8fd
Andrew Martins <andrew_omartins@yahoo.com>
2026-03-19 09:42:58 -0400

    i think thats how you write the comparator

--- coltSteele/dataStructures/fun.js
+++ coltSteele/dataStructures/fun.js
@@ -1,6 +1,7 @@
 
 
 function bubbleSort(arr, comparator){
+    let noSwap
     function swap(idx1, idx2){
         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
     }
@@ -10,4 +11,20 @@ function bubbleSort(arr, comparator){
             return a - b
         }
     }
+
+    for(let i = arr.length - 1; i > 0; i--){
+        noSwap = true
+        for(let j = 0; j < i - 1; j++){
+            if(comparator(arr[j], arr[j + 1]) > 0){
+                swap(arr, j, j + 1)
+                noSwap = false
+            }
+        }
+    }
+    return arr
+}
+
+
+function comparator(a , b){
+    return a > b ? 1 : a < b ? -1 : 0
 }
