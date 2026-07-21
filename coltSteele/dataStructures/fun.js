5dafb9e9a5f350ab7c89ddb28f63912c21d2897b
Andrew Martins <andrew_omartins@yahoo.com>
2026-07-21 00:18:40 -0400

    writing the patterns that i know based off of logic and understanding

--- coltSteele/dataStructures/fun.js
+++ coltSteele/dataStructures/fun.js
@@ -1,31 +1,3 @@
+//here we are going to test what i remember purely based of logic and understanding 
 
-
-function bubbleSort(arr, comparator){
-  
-    function swap(arr, idx1, idx2){
-        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-    }
-
-    if(typeof comparator != 'function'){
-        comparator = function(a ,b){
-            return a - b
-        }
-    }
-
-    for(let i = arr.length; i > 0; i--){
-        for(let j = 0; j < i - 1; j++){
-            if(comparator(arr[j], arr[j + 1]) > 0){
-                swap(arr, j, j + 1) 
-            }
-        }
-    }
-    return arr
-}
-
-
-function comparator(a , b){
-    return a > b ? 1 : a < b ? -1 : 0
-}
-
-
-console.table(bubbleSort([9,4,76,2,11,5,2,1]))
+//here we go!!
