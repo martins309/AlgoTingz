b51cff797250e4c37f2de1caee486aea9a1c883a
Andrew Martins <andrew_omartins@yahoo.com>
2026-07-21 00:23:04 -0400

    lets see if this works

--- coltSteele/dataStructures/fun.js
+++ coltSteele/dataStructures/fun.js
@@ -1,3 +1,19 @@
 //here we are going to test what i remember purely based of logic and understanding 
 
 //here we go!!
+
+
+
+
+function bubbleSort(arr) {
+    for(let i = arr.length; i > 0; i--) {
+        for(j = 0; j < i - 1; j++){
+            if(arr[j] < arr[j + 1]){
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    }
+    return arr
+}
+
+console.table(bubbleSort([9,3,57,4,8,3,1]))
